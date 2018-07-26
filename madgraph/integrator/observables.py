f41ab7fd356f63486bf20529262d0c6f5f00682e
Simone Lionetti <lionetti@itp.phys.ethz.ch>
2018-07-26 10:46:00 +0200

    Refined analysis for e+ e- collisions, added C-parameter and energy-energy correlation

--- madgraph/integrator/observables.py
+++ madgraph/integrator/observables.py
@@ -16,9 +16,6 @@
 from __future__ import division
 import os
 import logging
-import math
-import shutil
-import numpy as np
 
 try:
     import madgraph
@@ -44,36 +41,39 @@ logger = logging.getLogger('madgraph.integrator')
 pjoin = os.path.join
 
 class VirtualObservable(object):
-    """A mother base class that specified mandatory feature that any observable should implement."""
+    """Base class for observables."""
 
-    def __init__(self, name = 'default'):
+    def __init__(self, name='default'):
+
         self.name = name
-        pass    
 
     def __call__(self, wgt, *args, **opts):
-        """ Integrand function call, with list of continuous and discrete input values for all dimensions."""
+        """Integrand function call,
+        with list of continuous and discrete input values for all dimensions.
+        """
+
+        # This virtual function is currently abused for type checks
         assert(isinstance(wgt, float))
-        
-        # A unique float must be returned, but additional return values can 
-        # be specified in the body of the function or via options.
         return True
 
 class ObservableList(list):
-    """A mother base class that specified mandatory feature that any observable list should implement."""
+    """Base class for lists of observables."""
 
     def __init__(self, *args, **opts):
+
         super(ObservableList, self).__init__(*args, **opts)
 
     def apply_observables(self, wgt, *args, **opts):
-        """ Apply observables."""
+        """Apply all observables of this list."""
+
         for obs in self:
-            obs(wgt,*args, **opts)
+            obs(wgt, *args, **opts)
 
     def append(self, arg, **opts):
-        """ Type-checking. """
+        """Overload append for type-checking."""
+
         assert(isinstance(arg, VirtualObservable))
         super(ObservableList, self).append(arg, **opts)
-
 
 ##########################################################
 #                     HwU filling tools
@@ -86,7 +86,7 @@ class ObservableFunctions(object):
     @staticmethod
     def inclusive_xsec(*args,**kwargs):
         """Total cross section. No need to bin data by kinematics"""
-        return 1
+        return ((1, 1), )
 
     @staticmethod
     def scalar_pt_sum(data_for_observables,*args,**kwargs):
@@ -96,14 +96,17 @@ class ObservableFunctions(object):
         pt = 0
         for p in PS_point[len(flavors[0]):]:
             pt+=p.pt()
-        return pt
-
-
+        return ((pt, 1), )
 
 class HwUObservable(VirtualObservable,ObservableFunctions):
-    """Class that creates and fills in a HwU histogram for an observable given as a function"""
+    """Class that creates and fills in a HwU histogram
+    for an observable given as a function.
+    """
 
-    def __init__(self, name='default', observable_function = None, range = [0,2], nbins = 1,*args,**opts):
+    def __init__(
+        self, name='default', observable_function=None, range=(0, 2), nbins=1,
+        *args, **opts ):
+
         super(HwUObservable, self).__init__(name)
         if observable_function is None:
             self.observable_function = self.inclusive_xsec
@@ -115,22 +118,22 @@ class HwUObservable(VirtualObservable,ObservableFunctions):
         self.bin_size = (self.range[1]-self.range[0])/float(nbins)
         assert self.bin_size > 0
 
-        self.create_HwU()
+        self.create_HwU(**opts)
 
-    def create_HwU(self):
+    def create_HwU(self, **opts):
+
         bins = histograms.BinList(bin_range=self.range+[self.bin_size])
-        self.HwU = histograms.HwU(title=self.name,bins=bins)
+        self.HwU = histograms.HwU(title=self.name, bins=bins, **opts)
 
     def __call__(self, wgt, *args, **kwargs):
+
         assert super(HwUObservable, self).__call__(wgt, *args, **kwargs)
-        observable = self.observable_function(*args, **kwargs)
-        self.HwU.addEvent(observable, wgt)
-        #misc.sprint("ding")
-
-
-
+        values_and_weights = self.observable_function(*args, **kwargs)
+        for v, w in values_and_weights:
+            self.HwU.addEvent(v, wgt*w)
 
 class HwUObservableList(ObservableList):
+
     def __init__(self,*args,**opts):
         super(HwUObservableList, self).__init__(*args,**opts)
         assert all([isinstance(x,HwUObservable) for x in self])
@@ -143,7 +146,7 @@ class HwUObservableList(ObservableList):
         self.HwUList = histograms.HwUList(HwUs)
 
     def append(self, arg, **opts):
-        """ Type-checking. """
+
         assert(isinstance(arg, HwUObservable))
         super(ObservableList, self).append(arg, **opts)
 

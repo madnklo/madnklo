48d5d1f284e8ea8d7f922e489121f54f17f545f3
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2019-07-02 17:47:22 +0200

    working distributed qqQQ subtraction in the new framework -- lots of tidying up to do

--- madgraph/iolibs/template_files/subtraction/subtraction_schemes/distributed_soft_qqQQ/NNLO/QQqq_kernels.py
+++ madgraph/iolibs/template_files/subtraction/subtraction_schemes/distributed_soft_qqQQ/NNLO/QQqq_kernels.py
@@ -87,3 +87,143 @@ def C123C12_Qqqx_kernel(z1, z12, k1perp, k12perp, s12, s_12_3, s123):
     # ------------ TODO FIGURE OUT THIS STUFF ABOVE
 
     return C123C12_current# * jacobian * factor  TODO FIGURE OUT THIS STUFF
+
+
+def S12_qqx_PF_kernel(sir, sis, sik, skr, sks, srs):
+
+    sk_rs = skr + sks
+    si_rs = sir + sis
+    skrs = sk_rs + srs
+    sirs = si_rs + srs
+    frac = skrs / (sirs + skrs)
+    return 0.5*frac * 4 * (
+        (sir*sks + skr*sis - sik*srs) / (sirs*sk_rs)
+        - sir * sis / (sirs ** 2)
+        - skr * sks / (sk_rs ** 2)
+    ) / (srs ** 2)
+
+
+def C123_minus_C123S12_Qqqx_kernel(z1, z2, z3, s12, s13, s23):
+    s123 = s12 + s13 + s23
+    term1 = -s123**(-2)
+    term2 = (z1**2+z2**2)/(z1+z2)/(s12*s123)
+    return 2*(term1+term2)
+
+
+def S12C12_Qqqx_PF_kernel(z1, s12, p12hat, p3hat, p4hat, k1perp):
+
+    # # Rebuild the two-stage mapping
+    # interm_PS_point, interm_mapping_vars = self.get_intermediate_PS_point(
+    #     higher_PS_point, children )
+    # final__PS_point, final__mapping_vars = self.get_final_PS_point(
+    #     interm_PS_point, children )
+    #
+    # # Retrieve momenta
+    # p1 = higher_PS_point[children[0]]
+    # p2 = higher_PS_point[children[1]]
+    # p3 = higher_PS_point[emitter]
+    # p4 = higher_PS_point[spectator]
+    # p12  = p1 + p2
+    # p123 = p12 + p3
+    # p12hat = interm_PS_point[1000]
+    # p3hat = interm_PS_point[emitter]
+    # p4hat = interm_PS_point[spectator]
+    # p123hat = p12hat + p3hat
+    # p123tilde = final__PS_point[2000]
+    # Q = interm_mapping_vars['Q']
+
+    # Compute momentum fractions and transverse momenta
+    # zs, kTs = self.variables(higher_PS_point, p12hat, children[:2], Q=Q)
+    # z = zs[0] # ALL STEPS [0] [variables] [zs]
+    # kperp = kTs[0] # ALL STEPS [0] [variables] [kTs]
+
+    # Build scalar products
+    # s12  = p12.square() # Global variables ss
+    s12hat_3hat = 2*p3hat.dot(p12hat) # ALL STEPS [1] [variables] [ss]
+    s12hat_4hat = 2*p4hat.dot(p12hat) # need momenta
+    s3hat_4hat  = 2*p3hat.dot(p4hat) # need momenta
+    sperp3hat = 2*k1perp.dot(p3hat) # need momenta
+    sperp4hat = 2*k1perp.dot(p4hat) # need momenta
+    kperp2 = k1perp.square()
+    kperp_part = sperp3hat*sperp4hat/kperp2
+
+    # Construct the iterated current S(C(1,2))
+    eik_num = -s3hat_4hat
+    x = (sperp3hat*s12hat_4hat)/(sperp4hat*s12hat_3hat)
+    offdiag = 1. - 0.5*(x + 1./x)
+    cor_num = 2*z1*(1-z1)*kperp_part
+    den = s12*s12hat_3hat*s12hat_4hat
+    S12C12 = 2*(eik_num+cor_num*offdiag)/den
+
+    # Compute the partial fraction
+    frac = s12hat_4hat / (s12hat_3hat+s12hat_4hat)
+
+    # # If jacobians are active, correct the one-step jacobian to the two-step one
+    # try:
+    #     jacobian = opts['jacobian']
+    #     jacobian /= (final__mapping_vars['jacobian']*interm_mapping_vars['jacobian'])
+    # except KeyError:
+    #     jacobian = 1
+    # # Correct current factors if they are active
+    # factor_interm = self.factor(Q=Q, pC=p12, qC=p12hat)
+    # factor_final_ = self.factor(Q=Q, pC=p123hat, qC=p123tilde)
+    # factor_direct = self.factor(Q=Q, pC=p123, qC=p123tilde)
+    # factor = factor_interm * factor_final_ / factor_direct
+
+    return frac*S12C12
+
+
+def C123S12C12_Qqqx_PF_kernel(z1, z12hat, s12, p12hat, p3hat, k1perp, k12perphat):
+
+    # # Rebuild the two-stage mapping
+    # interm_PS_point, interm_mapping_vars = self.get_intermediate_PS_point(
+    #     higher_PS_point, children )
+    # final__PS_point, final__mapping_vars = self.get_final_PS_point(
+    #     interm_PS_point, children )
+    #
+    # # Retrieve momenta
+    # p1 = higher_PS_point[children[0]]
+    # p2 = higher_PS_point[children[1]]
+    # p3 = higher_PS_point[children[2]]
+    # p12 = p1 + p2
+    # p123 = p12 + p3
+    # p12hat = interm_PS_point[1000]
+    # p3hat = interm_PS_point[children[2]]
+    # p123hat = p12hat + p3hat
+    # p123tilde = final__PS_point[2000]
+    # Q = interm_mapping_vars['Q']
+
+    # # Compute momentum fractions and transverse momenta
+    # zs, kTs = self.variables(
+    #     higher_PS_point, p12hat, children[:2], Q=Q)
+    # zhats, kThats = self.variables(
+    #     interm_PS_point, p123tilde, (1000, children[2]), Q=Q)
+    # z = zs[0]
+    # kT = kTs[0]
+    # zhat = zhats[0]
+    # kThat = kThats[0]
+
+    # Build scalar products
+    # s12 = p12.square()
+    s12hat_3hat = 2*p3hat.dot(p12hat)
+    sTThat = 2*k1perp.dot(k12perphat)
+    kT2 = k1perp.square()
+    kThat2 = k12perphat.square()
+
+    # Construct the iterated current C(S(C(1,2)),3)
+    sqrbrk = 1. - z1*(1-z1)*sTThat*sTThat/(kT2*kThat2)
+    C123S12C12 = 8*(1-z12hat)/z12hat/(s12hat_3hat*s12)*sqrbrk
+
+    # # If jacobians are active, correct the one-step jacobian to the two-step one
+    # try:
+    #     jacobian = opts['jacobian']
+    #     jacobian /= (final__mapping_vars['jacobian']*interm_mapping_vars['jacobian'])
+    # except KeyError:
+    #     jacobian = 1
+    # # Correct current factors if they are active
+    # factor_interm = self.factor(Q=Q, pC=p12, qC=p12hat)
+    # factor_final_ = self.factor(Q=Q, pC=p123hat, qC=p123tilde)
+    # factor_direct = self.factor(Q=Q, pC=p123, qC=p123tilde)
+    # factor = factor_interm * factor_final_ / factor_direct
+
+    return C123S12C12
--- madgraph/iolibs/template_files/subtraction/subtraction_schemes/distributed_soft_qqQQ/NNLO/local_currents.py
+++ madgraph/iolibs/template_files/subtraction/subtraction_schemes/distributed_soft_qqQQ/NNLO/local_currents.py
@@ -20,25 +20,19 @@ import math
 import madgraph.core.subtraction as sub
 import madgraph.integrator.mappings as mappings
 
-#from commons.universal_kernels import AltarelliParisiKernels, SoftKernels #TODO DEV
-from madgraph.iolibs.template_files.subtraction.commons.universal_kernels import AltarelliParisiKernels, SoftKernels #TODO DEV
+from commons.universal_kernels import AltarelliParisiKernels, SoftKernels
 
-#import commons.utils as utils# TODO DEV
-import madgraph.iolibs.template_files.subtraction.commons.utils as utils #TODO DEV
+import commons.utils as utils
 
-#import commons.QCD_local_currents as currents #TODO DEV
-import madgraph.iolibs.template_files.subtraction.commons.QCD_local_currents as currents #TODO DEV
+import commons.QCD_local_currents as currents
 
-#import commons.factors_and_cuts as factors_and_cuts #TODO DEV
-import madgraph.iolibs.template_files.subtraction.commons.factors_and_cuts as factors_and_cuts #TODO DEV
+import commons.factors_and_cuts as factors_and_cuts
 
 from bidict import bidict
 
-import madgraph.iolibs.template_files.subtraction.subtraction_schemes.distributed_soft_qqQQ.distributed_soft_qqQQ_config as scheme_config # TODO DEV
-#import distributed_soft_qqQQ_config as scheme_config #TODO DEV
+import distributed_soft_qqQQ_config as scheme_config
 
-import madgraph.iolibs.template_files.subtraction.subtraction_schemes.distributed_soft_qqQQ.NNLO.QQqq_kernels as kernels # TODO DEV
-#import distributed_soft_qqQQ.NNLO.QQqq_kernels as kernels # TODO DEV
+import NNLO.QQqq_kernels as kernels
 
 import madgraph.various.misc as misc
 
@@ -124,10 +118,63 @@ class QCD_final_triple_collinear_qqxQ(QCD_final_collinear_0_XXX_soft_distrib):
         # (s_12hat_3hat,) = all_steps[1]['variables'][0]['ss']
 
         # Triple-collinear kernel
-        result = kernels.C123_Qqqx_kernel(z1,z2,z3,s12,s13,s23)
-        result *=  self.CF * self.TR
-        evaluation['values'][(0,0,0)] = {'finite':result}
+        result = kernels.C123_minus_C123S12_Qqqx_kernel(z1, z2, z3, s12, s13, s23)
+
+        # The color factor should be better handled
+        result *= self.CF * self.TR
+        evaluation['values'][(0,0,0)] = {'finite':0.5*result}
         return evaluation
+
+    def soft_kernel(self, evaluation, colored_parton_numbers, all_steps_info, global_variables):
+        # in Q -> Q q qx, the q qx pair (0,1) can go soft
+        # The parent is the double unresolved mapped Q, obtained after
+        # the full mapping (all_steps_info[1])
+        parent_ID = all_steps_info[1]['bundles_info'][0]['parent']
+        # Obtain the momenta of the Quark, quark and anti-quark
+        q_ID = global_variables['overall_children'][0][0]
+        qbar_ID = global_variables['overall_children'][0][1]
+        Q_ID = global_variables['overall_children'][0][2]
+        pq = all_steps_info[0]['higher_PS_point'][q_ID]
+        pqbar = all_steps_info[0]['higher_PS_point'][qbar_ID]
+        pQ = all_steps_info[0]['higher_PS_point'][Q_ID]
+
+        # Building the variables
+        # Final state invariants
+        (sqqbar,sqQ,sqbarQ) = global_variables['ss']
+        # Variables of the g->q qx splitting
+        z1_qq = all_steps_info[0]['variables'][0]['zs'][0]
+        k1perp = all_steps_info[0]['variables'][0]['kTs'][0]
+        # Variables of the Q->Qg splitting)
+        (p3hat,p12hat) = [all_steps_info[1]['higher_PS_point'][i] for i in all_steps_info[1]['bundles_info'][0]['final_state_children']]
+
+
+        # We loop over dipoles (k,parent) in the reduced process
+        # The dipoles are labelled by color_correlation_index
+        color_correlation_index = 1
+        for k in colored_parton_numbers:
+            if k == parent_ID:
+                # No color-diagonal emission
+                continue
+            else:
+                # Add the label (parent_ID,k) to the ordered list of
+                # emitting dipoles
+                evaluation['color_correlations'].append(((parent_ID, k),))
+                # Evaluate the eikonal factor
+                pk = all_steps_info[0]['higher_PS_point'][k]
+                sQk = 2*pk.dot(pQ)
+                sqk = 2*pk.dot(pq)
+                sqbark = 2*pk.dot(pqbar)
+                weight = 0
+                weight += kernels.S12_qqx_PF_kernel(sqQ, sqbarQ, sQk, sqk, sqbark, sqqbar)
+                weight -= kernels.S12C12_Qqqx_PF_kernel(z1_qq, sqqbar, p12hat, p3hat, pk, k1perp)
+                weight *= self.TR
+                evaluation['values'][(0, color_correlation_index,0)] = {'finite': weight}
+                color_correlation_index += 1
+
+
+        return evaluation
+
+
 
 class NoFinalNestedCollinear(QCD_final_collinear_0_XXX_soft_distrib):
     structure = [sub.SingularStructure(sub.CollStructure(
@@ -194,8 +241,47 @@ class NoFinalDoubleSoftCollinearinTripleCollinear(QCD_final_collinear_0_XXX_soft
 
     # mapping_rules = QCD_final_collinear_0_XXX_soft_distrib.mapping_rules[:]
     # mapping_rules[0]['singular_structure'] = structure[0]
+    # --------------------------- TODO DEV
+    # TMP
+    # --------------------------- TODO DEV
+    mapping_rules = QCD_final_collinear_0_XXX_soft_distrib.mapping_rules[:]
+    mapping_rules[0]['singular_structure'] = sub.SingularStructure(sub.CollStructure(
+        sub.SubtractionLeg(0, +1, sub.SubtractionLeg.FINAL),
+        sub.SubtractionLeg(1, -1, sub.SubtractionLeg.FINAL),),)
+    mapping_rules[1]['singular_structure'] = sub.SingularStructure(sub.CollStructure(
+        sub.SubtractionLeg(1001, 21, sub.SubtractionLeg.FINAL),
+        sub.SubtractionLeg(2, +2, sub.SubtractionLeg.FINAL),),)
 
-    is_zero = True
+    def kernel(self, evaluation, all_steps, global_variables):
+        """TODO DEV COMBINE ABOVE AFTER DEBUG Combining the whole qbar-q-Q forest
+        The implementation is not memory-cautious at all but aims at readability
+        """
+        # Variables of the triple splitting (Q->Q q qbar)
+        (z1,z2,z3)=global_variables['zs']
+        (s12,s13,s23)=global_variables['ss']
+        (k1perp, k2perp, k3perp) = global_variables['kTs']
+
+        # Variables of the nested double splitting (g->q qbar)
+        (z1_qq,z2_qq) = all_steps[0]['variables'][0]['zs']
+        (k1perp_qq,k2perp_qq) = all_steps[0]['variables'][0]['kTs']
+        (s12_qq,) = all_steps[0]['variables'][0]['ss'] #This is the same as s12 above
+
+        # Variables of the mapped double splitting (Q->Qg)
+        (z3hat,z12hat) = all_steps[1]['variables'][0]['zs']
+        (k3perphat,k12perphat) = all_steps[1]['variables'][0]['kTs']
+        (s_12hat_3hat,) = all_steps[1]['variables'][0]['ss']
+        (p3hat,p12hat) = [all_steps[1]['higher_PS_point'][i] for i in all_steps[1]['bundles_info'][0]['final_state_children']]
+        # Triple-collinear kernel
+        result = 0.25*kernels.C123S12C12_Qqqx_PF_kernel(z1_qq,z12hat,s12,p12hat,p3hat,k1perp_qq,k12perphat)
+        result *=  self.CF * self.TR
+        evaluation['values'][(0,0,0)] = {'finite':result}
+        return evaluation
+
+    # --------------------------- TODO DEV
+
+
+
+    #is_zero = True TODO DEV
 
 class NoFinalDoubleSoft(QCD_final_collinear_0_XXX_soft_distrib):
     structure = [sub.SingularStructure(

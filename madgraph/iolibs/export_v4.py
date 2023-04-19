9c7cba454bcbd752dc97f266805b5a32b935d3a7
Gloria Bertolotti <gloria@Glorias-MacBook-Air.local>
2023-04-19 16:23:31 +0200

    Copy driver_n in LO directory

--- madgraph/iolibs/export_v4.py
+++ madgraph/iolibs/export_v4.py
@@ -2219,7 +2219,7 @@ class ProcessExporterFortranSA(ProcessExporterFortran):
         user_linkfiles = [] 
         if strdirpath[-1][0] == 'L': # These links need to exist only for LO_XXXX directories
                                      # For the NLO_XXXX we have a makefile for each Subprocess
-            user_linkfiles = ['makefile_n', 'LO_B.f']
+            user_linkfiles = ['driver_n.f','makefile_n', 'LO_B.f']
 
         for file in linkfiles:
             ln('../%s' % file, cwd=dirpath)

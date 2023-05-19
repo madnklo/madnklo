accf196a9df98b29e20e137cbe5fafdb7a639339
Gloria Bertolotti <gloria@Glorias-MacBook-Air.local>
2023-05-19 14:32:36 +0200

    Template files for virtual directory, as NLO_I, NLO_V, makefile; temporary phase-space function for running Born and Virtual in 3-jet processes

---
+++ tmp_fortran/tmp_files/NLO_I_template.f
@@ -0,0 +1,152 @@
+      subroutine int_counter_NLO(p,sLO,INLO,ierr)
+c     MSbar finite part of the integrated counterterm
+      implicit none
+      INCLUDE 'nexternal.inc'
+      INCLUDE 'math.inc'
+      INCLUDE 'damping_factors.inc'
+      INCLUDE 'nsqso_born.inc'
+      INCLUDE 'coupl.inc'
+      INCLUDE 'input.inc'
+      INCLUDE 'virtual_recoilers.inc'
+      INCLUDE 'leg_PDGs_%(proc_prefix)s.inc'
+      INCLUDE 'colored_partons.inc'
+      integer i,j,r
+      integer ierr
+      double precision p(0:3,nexternal)
+      double precision sLO(nexternal,nexternal)
+      double precision INLO,pref
+      double precision BLO,ccBLO
+      double precision A20a,A21a,A20b,A20,A21
+      DOUBLE PRECISION ALPHAS,ANS(0:NSQSO_BORN)
+      DOUBLE PRECISION ALPHA_QCD
+      INTEGER, PARAMETER :: HEL = - 1
+      DOUBLE PRECISION  GET_CCBLO
+      integer iref1(nexternal)
+c
+c     initialise
+      ALPHAS=ALPHA_QCD(AS,NLOOP,MU_R)
+      pref=alphas/(2d0*pi)
+      INLO=0d0
+      iref1 = 0
+      CCBLO = 0d0
+      BLO = 0d0
+
+      CALL ME_ACCESSOR_HOOK(P,HEL,ALPHAS,ANS)
+      BLO = ANS(0)
+c      TODO: modify ierr      
+c      if(ierr.eq.1)goto 999
+
+c     TODO: add check 
+      do i=1,len_iref
+         iref1(iref(1,i)) = iref(2,i)
+      enddo
+
+c
+c     Born contribution
+      do i=1,nexternal
+         if(leg_pdgs_%(proc_prefix)s(i).eq.21) then
+            INLO=INLO+(CA/6d0+2*TR*Nf/3d0)*(log(sLO(i,iref1(i))/MU_R**2)-8d0/3d0)+CA*(6d0-7d0/2d0*zeta2)
+c           factor for conversion torino to ML, namely gamma[1-eps] to exp[- eps eulergamma]      
+            INLO = INLO + pi**2/12d0 * CA
+         elseif(leg_pdgs_%(proc_prefix)s(i).ne.0 .and.abs(leg_pdgs_%(proc_prefix)s(i)).le.6) then
+            INLO=INLO+(CF/2d0)*(10d0-7d0*zeta2+log(sLO(i,iref1(i))/MU_R**2))
+c           factor for conversion torino to ML, namely gamma[1-eps] to exp[- eps eulergamma]  
+            INLO = INLO + pi**2/12d0 * CF
+         endif
+      enddo
+
+c
+c     Include damping factors
+      A20a=A20(alpha)
+      A21a=A21(alpha)
+      A20b=A20(beta_FF)
+      do i=1,nexternal
+         if(leg_pdgs_%(proc_prefix)s(i).eq.21)INLO=INLO+CA*(A20a*(A20a-2d0*A20b)-A21a)+(gamma_g-2d0*CA)*A20b
+         if(leg_pdgs_%(proc_prefix)s(i).ne.0 .and.abs(leg_pdgs_%(proc_prefix)s(i)).le.6)INLO=INLO+CF*(A20a*(A20a-2d0*A20b)-A21a)+(gamma_q-2d0*CF)*A20b
+      enddo
+
+      INLO=INLO*BLO
+c
+c     Colour-linked-Born contribution
+      do i=1,nexternal
+         do j=1,nexternal
+            if(ISLOQCDPARTON(i) .and. ISLOQCDPARTON(j)) then
+               CCBLO = GET_CCBLO(i,j)
+            else
+               cycle
+            endif
+            if(j.eq.i)cycle
+            INLO=INLO+ccBLO*log(sLO(i,j)/MU_R**2)*(2d0-log(sLO(i,j)/MU_R**2)/2d0)
+         enddo
+      enddo
+      INLO=INLO*pref
+c
+      if(abs(INLO).ge.huge(1d0).or.isnan(INLO))then
+         write(77,*)'Exception caught in int_counter_NLO',INLO
+         goto 999
+      endif
+
+c
+      return
+ 999  ierr=1
+      return
+      end
+
+
+      function A10(w)
+c 
+c     A10(w) = Psi0(w+1) + eulergamma
+      implicit none
+      double precision A10,w
+c
+      if(w.ne.0d0.and.w.ne.1d0.and.w.ne.2d0.and.w.ne.3d0)then
+         write(*,*)'Value not coded in A10',w
+         stop
+      endif
+c
+      if(w.eq.0d0)A10=0d0
+      if(w.eq.1d0)A10=1d0
+      if(w.eq.2d0)A10=3d0/2d0
+      if(w.eq.3d0)A10=11d0/6d0
+c 
+      return
+      end
+
+      function A20(w)
+c 
+c     A20(w) = Psi0(w+2) - 1 + eulergamma
+      implicit none
+      double precision A20,w
+c
+      if(w.ne.0d0.and.w.ne.1d0.and.w.ne.2d0.and.w.ne.3d0)then
+         write(*,*)'Value not coded in A20',w
+         stop
+      endif
+c
+      if(w.eq.0d0)A20=0d0
+      if(w.eq.1d0)A20=1d0/2d0
+      if(w.eq.2d0)A20=5d0/6d0
+      if(w.eq.3d0)A20=13d0/12d0
+c 
+      return
+      end
+
+
+      function A21(w)
+c 
+c     A21(w) = Psi1(w+2) + 1 - Zeta2
+      implicit none
+      double precision A21,w
+c
+      if(w.ne.0d0.and.w.ne.1d0.and.w.ne.2d0.and.w.ne.3d0)then
+         write(*,*)'Value not coded in A21',w
+         stop
+      endif
+c
+      if(w.eq.0d0)A21=0d0
+      if(w.eq.1d0)A21=-1d0/4d0
+      if(w.eq.2d0)A21=-13d0/36d0
+      if(w.eq.3d0)A21=-61d0/144d0
+c 
+      return
+      end
---
+++ tmp_fortran/tmp_files/NLO_V_template.f
@@ -0,0 +1,141 @@
+      function int_virtual(x,wgt)
+c     n-body NLO integrand for vegas
+      implicit none
+      include 'math.inc'
+      include 'nexternal.inc'
+      INCLUDE 'coupl.inc'
+      INCLUDE 'input.inc'
+      INCLUDE 'run.inc'
+      INCLUDE 'cuts.inc'
+      INCLUDE 'ngraphs.inc'
+      INCLUDE 'nsqso_born.inc'
+      INCLUDE 'nsquaredSO.inc'
+      integer ndim,ierr,ievt,nthres,i
+      save ievt,nthres
+      integer nitV
+      common/niterationsv/nitV
+      double precision int_virtual,VNLO,INLO,VNLO_MG
+      double precision sLO(nexternal,nexternal)
+c     TODO: understand x(mxdim) definition by Vegas
+      integer, parameter :: mxdim = 30
+      double precision x(mxdim)
+      double precision wgt,wgtpl
+      logical doplot, docut
+      common/cdoplot/doplot
+      double precision p(0:3,nexternal)
+      double precision xjac
+      double precision sCM
+      double precision ans(0:1) !TODO SET CORRECTLY RANGE OF ANS 
+      double precision alphas, alpha_qcd
+      integer, parameter :: hel=-1
+      LOGICAL INIT
+      DATA INIT/.TRUE./
+      COMMON/INITCHECKSA/INIT
+      INTEGER MATELEM_ARRAY_DIM
+      REAL*8 , ALLOCATABLE :: MATELEM(:,:)
+      REAL*8 SQRTS,AO2PI,TOTMASS
+C     sqrt(s)= center of mass energy 
+      REAL*8 PIN(0:3), POUT(0:3)
+      CHARACTER*120 BUFF(NEXTERNAL)
+      INTEGER RETURNCODE, UNITS, TENS, HUNDREDS
+      INTEGER NSQUAREDSO_LOOP
+      REAL*8 , ALLOCATABLE :: PREC_FOUND(:)
+      REAL * 8 finite_part, single_pole, double_pole, BLO
+
+C     
+C     GLOBAL VARIABLES
+C     
+C     This is from ML code for the list of split orders selected by
+C     the process definition
+C     
+      INTEGER NLOOPCHOSEN
+      CHARACTER*20 CHOSEN_LOOP_SO_INDICES(NSQUAREDSO)
+      LOGICAL CHOSEN_LOOP_SO_CONFIGS(NSQUAREDSO)
+      COMMON/%(long_proc_prefix)sCHOSEN_LOOP_SQSO/CHOSEN_LOOP_SO_CONFIGS
+      INTEGER NBORNCHOSEN
+      CHARACTER*20 CHOSEN_BORN_SO_INDICES(NSQSO_BORN)
+      LOGICAL CHOSEN_BORN_SO_CONFIGS(NSQSO_BORN)
+      COMMON/%(long_proc_prefix)sCHOSEN_BORN_SQSO/CHOSEN_BORN_SO_CONFIGS
+
+C     
+C     EXTERNAL
+C     
+c     TODO: convert to partonic sCM 
+      sCM = (2d0*EBEAM(1))**2
+c     TODO: muR from card
+      ALPHAS=ALPHA_QCD(AS,NLOOP,MU_R)
+
+c
+c     initialise
+      xjac=0d0
+      int_virtual=0d0
+      sLO=0d0
+      VNLO = 0d0
+      INLO = 0d0
+c
+
+C     
+C     BEGIN CODE
+C     
+C     
+
+
+      IF (INIT) THEN
+        INIT=.FALSE.
+        CALL %(long_proc_prefix)sGET_ANSWER_DIMENSION(MATELEM_ARRAY_DIM)
+        ALLOCATE(MATELEM(0:3,0:MATELEM_ARRAY_DIM))
+        CALL %(long_proc_prefix)sGET_NSQSO_LOOP(NSQUAREDSO_LOOP)
+        ALLOCATE(PREC_FOUND(0:NSQUAREDSO_LOOP))
+!        INCLUDE 'pmass.inc'
+      ENDIF
+
+
+c     phase space and invariants
+      if(sCM.le.0d0)then
+         write(*,*) 'Wrong sCM', sCM
+         stop
+      endif
+      call phase_space_n(x,sCM,p,nexternal,xjac)
+      if(xjac.eq.0d0)goto 999
+      call invariants_from_p(p,nexternal,sLO,ierr)
+      if(ierr.eq.1)goto 999
+c
+c     possible cuts
+c     if(docut(p,nexternal))goto 999
+c
+c     virtual
+      CALL %(long_proc_prefix)sSLOOPMATRIX_THRES(p,MATELEM,-1.0D0,PREC_FOUND,RETURNCODE)
+
+      finite_part = MATELEM(1,0)
+      single_pole = MATELEM(2,0)
+      double_pole = MATELEM(3,0)
+
+      VNLO = finite_part
+      
+      CALL ME_ACCESSOR_HOOK(P,HEL,ALPHAS,ANS)
+      BLO = ANS(0)
+      
+      if(ierr.eq.1)goto 999
+c
+c     counterterm
+      call int_counter_NLO(p,sLO,INLO,ierr)
+      if(ierr.eq.1)goto 999
+c
+c     subtraction
+      int_virtual=(VNLO+INLO)*xjac
+      int_virtual = int_virtual/2d0/sCM
+c
+c     plot
+      wgtpl=int_virtual*wgt/nitV
+      if(doplot)call histo_fill(p,sLO,nexternal,wgtpl)
+c
+c     print out current run progress
+c     999  ievt=ievt+1
+c      if(ievt.gt.nthres)then
+c         write(*,111)char(13),int(1d2*nthres/(nprodV*1d0)),' done'
+c         nthres=nthres+int(nprodV/rfactV)
+c      endif
+c 111  format(a1,i3,a6,$)
+c
+ 999  return
+      end

76377a80e1bd4fc5750ba9581ef9c2a15e715149
Paolo Torrielli <paolotorrielli@to4161mc.to.infn.it>
2023-07-04 18:00:06 +0200

    updates in phase space for real (ongoing)

--- Template/Fortran_tmp/src_to_common/gen_real_phase_space.f
+++ Template/Fortran_tmp/src_to_common/gen_real_phase_space.f
@@ -1,6 +1,7 @@
       subroutine phase_space_npo(x,shat,iU,iS,iB,iA,p,pbar,xjac,xjacB)
 c     iU is the unresolved parton associated with the soft singularity
       implicit none
+      include 'coupl.inc'
       include 'math.inc'
       include 'nexternal.inc'
       include 'leg_PDGs.inc'
@@ -8,14 +9,22 @@ c     iU is the unresolved parton associated with the soft singularity
       double precision p(0:3,nexternal),pbar(0:3,nexternal-1)
       double precision xjac,xjacB,xjacCS
       integer i,j,iU,iS,iB,iA
+      integer iconfig,mincfig,maxcfig,invar
 c
 c     initialise
       p=0d0
       pbar=0d0
-      xjac=0d0
+      xjacB=Gevtopb
 c
-c     call Born phase space
-      call phase_space_n(x(4),shat,pbar,nexternal-1,xjacB)
+C     Hard coded settings for gen_mom
+      iconfig = 1
+      mincfig = 1
+      maxcfig = 1
+      invar = 2
+      call gen_mom(iconfig,mincfig,maxcfig,invar,xjacB,x(4),pbar)
+c$$$c
+c$$$c     call Born phase space
+c$$$      call phase_space_n(x(4),shat,pbar,nexternal-1,xjacB)
 c
 c     call radiation phase space
       call phase_space_CS(x(1),iU,iS,iB,iA,p,pbar,nexternal,leg_PDGs,'C',xjacCS)

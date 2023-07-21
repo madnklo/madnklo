fda1891802edca58d89b5a104d99468bf108c440
Giovanni Limatola <giovannilimatola@MacBook-Pro-di-Giovanni-2.local>
2023-07-21 16:06:34 +0200

    driver_n.f to implement multi channeling

--- Template/Fortran_tmp/src_to_common/driver_n.f
+++ Template/Fortran_tmp/src_to_common/driver_n.f
@@ -6,6 +6,7 @@
       INCLUDE 'run.inc'
       INCLUDE 'cuts.inc'
       INCLUDE 'colored_partons.inc'
+      INCLUDE 'ngraphs.inc'
       integer mxdim
       parameter(mxdim=30)
       integer ndim,i,j,idum
@@ -30,6 +31,13 @@ c     vegas declarations
       double precision region(2*mxdim),xi(ndmx,mxdim)
       parameter(acc=1d-10)
       common/rand/idum
+      integer ich
+      common/comich/ich
+      double precision sum_b,sum_errb
+      sum_b=0d0
+      sum_errb=0d0
+      res_b=0d0
+      err_b=0d0
 c
       call SETPARA('param_card.dat')
       call SETRUN('run_card.dat')
@@ -60,52 +68,66 @@ c     phase-space dimension, same for all contributions to this folder
          region(i+ndim)=1d0
       enddo
 
-      open(unit=iu1,file='integration_B.log')
-      open(unit=iu7,file='failures_B.log')
+      do i=1,N_MAX_CG
+         ich=i
+      
+         open(unit=iu1,file='integration_B.log')
+         open(unit=iu7,file='failures_B.log')
 
-      write(*,'(a)')'Warm up Born'
-      write(iu1,'(a)')'============================='
-      write(iu1,'(a)')' BORN WARMUP                 '
-      write(iu1,'(a)')'============================='
-      init=0
-      doplot=.false.
-      call vegas(region,ndim,int_Born,init,nclBth,nitBth,nprn,res_b,err_b,chi2a,acc,xi,it,ndo,si,swgt,schi)
-c
-      write(*,'(a)')'Integrating Born'
-      write(iu1,'(a)')
-      write(iu1,'(a)')'============================='
-      write(iu1,'(a)')' BORN                        '
-      write(iu1,'(a)')'============================='
-      init=1
-      doplot=.true.
-      call histo_init
-      call vegas(region,ndim,int_Born,init,nclB,nitB,nprn,res_b,err_b,chi2a,acc,xi,it,ndo,si,swgt,schi)
-      rescale_plot_B=dble(nitB)/min(dble(nitB),dble(it))
-      write(*,110)char(13),'...done     '
-      write(*,*)
+         write(*,'(a)')'Warm up Born'
+         write(iu1,'(a)')'============================='
+         write(iu1,'(a)')' BORN WARMUP                 '
+         write(iu1,'(a)')'============================='
+         init=0
+         doplot=.false.
+         call vegas(region,ndim,int_Born,init,nclBth,nitBth,nprn,res_b,err_b,chi2a,acc,xi,it,ndo,si,swgt,schi)
+
+         write(*,'(a)')'Integrating Born'
+         write(iu1,'(a)')
+         write(iu1,'(a)')'============================='
+         write(iu1,'(a)')' BORN                        '
+         write(iu1,'(a)')'============================='
+         init=1
+         doplot=.true.
+         call histo_init
+         call vegas(region,ndim,int_Born,init,nclB,nitB,nprn,res_b,err_b,chi2a,acc,xi,it,ndo,si,swgt,schi)
+         rescale_plot_B=dble(nitB)/min(dble(nitB),dble(it))
+         write(*,110)char(13),'...done     '
+         write(*,*)
+
+         sum_b = sum_b + res_b
+         write(*,*) 'res_b = ', res_b, err_b
+         write(*,*)
+         sum_errb = sum_errb + err_b**2
+c         call histo_final('plot_B.dat',rescale_plot_B)
+c     
+c     
+      enddo
+
       call histo_final('plot_B.dat',rescale_plot_B)
-c
       open(unit=iu,file='results_B.log')
-      line='=================================================='
-      write(iu,*)' B '
-      write(iu,*)
-      write(iu,*)' itns and calls for B warmup  = ',nitBth,nclBth
-      write(iu,*)' itns and calls for B integration = ',nitB,nclB
-      write(iu,*)
-c      write(iu,*)' '//line//line
-      write(iu,*)' '//line
-      write(iu,*)' '//line
-      write(iu,*)' sigma B [pb]  = ',res_b,' +-',err_b
-      write(iu,*)' '//line
-      write(iu,*)' '//line
-c      write(iu,*)' '//line//line
-      write(iu,*)
-      close(iu)
-      close(iu1)
-      close(iu7)
-c
-c
- 110  format(a1,a12,$)
-c
+         line='=================================================='
+         write(iu,*)' B '
+         write(iu,*)
+         write(iu,*)' itns and calls for B warmup  = ',nitBth,nclBth
+         write(iu,*)' itns and calls for B integration = ',nitB,nclB
+         write(iu,*)
+c     write(iu,*)' '//line//line
+         write(iu,*)' '//line
+         write(iu,*)' '//line
+C         write(iu,*)' sigma B [pb]  = ',res_b,' +-',err_b
+         write(iu,*)' sigma B [pb]  = ',sum_b,' +-',dsqrt(sum_errb)
+         write(iu,*)' '//line
+         write(iu,*)' '//line
+c     write(iu,*)' '//line//line
+         write(iu,*)
+         close(iu)
+         close(iu1)
+         close(iu7)
+c     
+c     
+ 110     format(a1,a12,$)
+
+      
       stop
       end

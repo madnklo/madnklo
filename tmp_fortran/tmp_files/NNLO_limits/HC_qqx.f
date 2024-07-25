38eec4fafe0f96ded9053d76850c4f21e631808f
Gloria Bertolotti <gloria@Glorias-MacBook-Air.local>
2024-07-25 12:38:51 +0200

    Added NNLO_limits dir

---
+++ tmp_fortran/tmp_files/NNLO_limits/HC_qqx.f
@@ -0,0 +1,123 @@
+
+
+      double precision function M2_HC_qqx(ia,ib,ir,xs,xp,xsb,xpb,wgt,xj,nit,extra,wgt_chan,ierr)
+c     hard-collinear limit C_(ia,ib)
+c     this is meant to represent the full hard-collinear
+c     for sectors (ia,ib)+(ib,ia)
+      implicit none
+      include 'nexternal.inc'
+      INCLUDE 'coupl.inc'
+      include 'math.inc'
+      include 'damping_factors.inc'
+      include 'nsqso_born.inc'
+      include 'leg_PDGs.inc'
+      INCLUDE 'input.inc'
+      INCLUDE 'run.inc'      
+      integer ia,ib,ir,ierr,nit,parent_leg
+      double precision pref,M2tmp,wgt,wgtpl,wgt_chan,xj,extra
+      double precision xs(nexternal,nexternal),xsb(nexternal-1,nexternal-1)
+      double precision BLO,KKBLO
+      double precision xp(0:3,nexternal),xpb(0:3,nexternal-1),kt(0:3)
+      double precision sab,sar,sbr,x,y,xinit,damp
+      double precision wa,wb,wr
+      double precision ANS(0:NSQSO_BORN)
+      integer mapped_labels(nexternal),mapped_flavours(nexternal)
+      integer, parameter :: hel = - 1
+c     set logical doplot
+      logical doplot
+      common/cdoplot/doplot
+      double precision sCM
+      common/cscm/sCM
+      logical docut
+      integer %(proc_prefix_real)s_fl_factor
+      common/%(proc_prefix_real)s_flavour_factor/%(proc_prefix_real)s_fl_factor
+      double precision alphas,alpha_qcd
+      double precision %(proc_prefix_HC_qqx)s_get_kkblo
+      integer %(proc_prefix_real)s_den
+      common/%(proc_prefix_real)s_iden/%(proc_prefix_real)s_den
+      integer %(proc_prefix_HC_qqx)s_den
+      common/%(proc_prefix_HC_qqx)s_iden/%(proc_prefix_HC_qqx)s_den
+      INTEGER ISEC,JSEC
+      COMMON/CNLOSECINDICES/ISEC,JSEC
+      INTEGER BORN_LEG_PDGS(NEXTERNAL-1)
+c
+c     initialise
+      M2_HC_qqx=0d0
+      M2tmp=0d0
+      ierr=0
+      damp=0d0
+c
+c     possible cuts
+      call GET_BORN_PDGS(ISEC,JSEC,NEXTERNAL-1,BORN_LEG_PDGS)
+      IF(DOCUT(XPB,NEXTERNAL-1,BORN_LEG_PDGS,0))RETURN
+
+
+c
+c     overall kernel prefix
+      alphas=alpha_QCD(asmz,nloop,scale)
+      pref=8d0*pi*alphas
+c
+c     invariant quantities
+      sab=xs(ia,ib)
+      sar=xs(ia,ir)
+      sbr=xs(ib,ir)
+      x=sar/(sar+sbr)
+      y=sab/(sab+sar+sbr)
+      xinit = 1d0 - sab/(sar+sbr)
+c
+c     coefficients of kt
+c     kt = wa pa + wb pb + wr pr
+      wa= 1d0 - x
+      wb= - x
+      wr= - (1d0-2d0*x)*sab/(sar+sbr)
+      kt(:) = wa*xp(:,ia) + wb*xp(:,ib) + wr*xp(:,ir) 
+c
+c     safety check
+      if(sab.le.0d0.or.sar+sbr.le.0d0.or.x.le.0d0.or.x.ge.1d0)then
+         write(77,*)'Inaccuracy 1 in M2_HC_qqx',sab,sar+sbr,x
+         goto 999
+      endif
+c
+c     call Born
+      call %(proc_prefix_HC_qqx)s_ME_ACCESSOR_HOOK(xpb,hel,alphas,ANS)
+      BLO = ANS(0)
+c
+      call get_collinear_mapped_labels(ia,ib,ir,nexternal,leg_PDGs,mapped_labels,mapped_flavours)
+      parent_leg = mapped_labels(ib)
+      if(mapped_flavours(ib).ne.21)then
+         write(*,*) 'Wrong parent particle label!', ib, mapped_flavours(ib)
+         stop
+      endif
+c
+      KKBLO = %(proc_prefix_HC_qqx)s_GET_KKBLO(parent_leg,xpb,kt)
+c     TODO: improve ktmuktnuBmunu / kt^2
+      M2tmp=TR*(BLO-4d0/sab*KKBLO)
+c     Including correct multiplicity factor
+      M2tmp = M2tmp*dble(%(proc_prefix_HC_qqx)s_den)/dble(%(proc_prefix_real)s_den)
+c     account for different damping factors according to
+c     recoiler position (ir)
+      if(ir.ge.2)then
+         damp=(1d0-y)**beta_FF
+      else
+         damp=xinit**beta_FI
+      endif
+      M2tmp=M2tmp*damp
+      M2_HC_qqx=M2tmp*pref/sab*xj*extra
+c     apply flavour factor
+      M2_HC_qqx=M2_HC_qqx*%(proc_prefix_real)s_fl_factor
+c
+c     plot
+      wgtpl=-M2_HC_qqx*wgt/nit*wgt_chan
+      if(doplot)call histo_fill(xpb,xsb,nexternal-1,wgtpl)
+c
+c     sanity check
+      if(abs(M2_HC_qqx).ge.huge(1d0).or.isnan(M2_HC_qqx))then
+         write(77,*)'Exception caught in M2_HC_qqx',M2_HC_qqx
+         goto 999
+      endif
+c
+      return
+ 999  ierr=1
+      return
+      end
+
---
+++ tmp_fortran/tmp_files/NNLO_limits/test_ct.f
@@ -0,0 +1,34 @@
+c proc_prefix_rr
+	%(proc_prefix_rr)s
+
+c indices
+	%(isec)s, %(jsec)s, %(ksec)s, %(lsec)s
+
+c proc_prefix_S_g
+	%(proc_prefix_S_g)s
+
+c proc_prefix_HC_g
+        %(proc_prefix_HC_gg)s
+        %(proc_prefix_HC_gq)s
+        %(proc_prefix_HC_qqx)s
+
+c proc_prefix_SS
+        %(proc_prefix_SS_gg)s
+        %(proc_prefix_SS_qqx)s
+
+c proc_prefix_SHC
+	%(proc_prefix_SHC_ggg)s
+        %(proc_prefix_SHC_ggq)s
+        %(proc_prefix_SHC_gqqx)s
+
+c proc_prefix_HCC
+        %(proc_prefix_HCC_ggg)s
+        %(proc_prefix_HCC_ggq)s
+        %(proc_prefix_HCC_gqqx)s
+        %(proc_prefix_HCC_qxqq)s
+        %(proc_prefix_HCC_qxqqp)s
+
+c proc_prefix_CCSHC
+        %(proc_prefix_CCSHC_ggg)s
+        %(proc_prefix_CCSHC_ggq)s
+        %(proc_prefix_CCSHC_gqqx)s

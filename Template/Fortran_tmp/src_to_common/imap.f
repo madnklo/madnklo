9397e576960523dab4d6cd7f6864feababd9126b
Giovanni Limatola <giovannilimatola@MBPdiGiovanni5.homenet.telecomitalia.it>
2024-07-30 19:29:24 +0200

    Work on imap.f

--- Template/Fortran_tmp/src_to_common/imap.f
+++ Template/Fortran_tmp/src_to_common/imap.f
@@ -77,7 +77,7 @@ c     exclude the mapped_flavours=0 value of the removed gluon
       subroutine get_collinear_mapped_labels(a,b,c,n,leg_pdgs,
      $           mapped_labels,mapped_flavours)
       implicit none
-      include 'virtual_recoilers.inc'
+c      include 'virtual_recoilers.inc'
       integer i,j
       integer a,b,c,n
       integer isec,jsec
@@ -98,6 +98,7 @@ c     initialise
       mapped_labels = 0
       mapped_flavours = 0
 
+
 c     check mapping structure
       if(a.le.2)then
          write(*,*) 'get_collinear_mapped_labels: '
@@ -108,32 +109,72 @@ c     check mapping structure
       endif
 
 c     Associate a,b,c to the collinear particles in the sector
-      if(a.eq.isec) then
-         rm_leg = a
-         if(b.eq.jsec) then
+
+c      write(*,*) 'a, b, c   = ', a, b, c
+c      write(*,*) 'isec,jsec = ',isec,jsec
+c     pause
+
+      
+      rm_leg = a
+
+      if(ksec.eq.0 .and. lsec .eq.0) then ! This is NLO mapping
+         if(a.eq.isec.and.b.eq.jsec) then
             parent_leg = b
             rec_leg = c
-         elseif(c.eq.jsec) then
-            parent_leg = c
-            rec_leg = b
+         else
+            write(*,*) 'imap : NLO mapping error...'
+            write(*,*) 'It should be a = isec, b = jsec'
+            write(*,*) 'a, b, isec, jsec = ', a, b, isec, jsec
+            stop
          endif
-      elseif(a.eq.jsec) then
-         rm_leg = a
-         if(b.eq.isec) then
+      elseif(ksec.ne.0 .and. lsec .eq. 0) then ! NNLO Mapping for 3 index sector (i,j,k)
+         if((a.eq.isec.and.b.eq.jsec).or.(a.eq.jsec .and. b.eq.ksec)) then
             parent_leg = b
             rec_leg = c
-         elseif(c.eq.isec) then
-            parent_leg = c
-            rec_leg = b
+         else
+            write(*,*) 'imap : NNLO mapping error...'
+            write(*,*) 'It should be a = isec, b = jsec
+     $       or a = jsec, b = ksec'
+            write(*,*) 'a, b, isec, jsec, ksec = ', a, b, isec, jsec, ksec
+            stop
          endif
-      elseif((rm_leg.eq.0).or.(parent_leg.eq.0).or.(rm_leg.eq.parent_leg)) then
-         write(*,*) 'get_collinear_mapped_labels: '
-         write(*,*) 'Mapping tuple (abc) does not include'
-         write(*,*) 'particles defining the singular sector (ij) !'
-         write(*,*) 'a, b, c = ', a, b, c
-         write(*,*) 'i, j = ', isec, jsec
+      elseif(ksec.ne.0 .and. lsec .ne. 0) then ! NNLO Mapping for 4 index sector (i,j,k,l)
+         write(*,*) 'NNLO mapping for 4 index sector to be implemented'
          stop
       endif
+c$$$      elseif((a.eq.isec.and.c.eq.jsec).or.(a.eq.jsec.and.c.eq.isec)) then
+c$$$         parent_leg = c
+c$$$         rec_leg = b
+c$$$      endif
+
+c$$$      
+c$$$      
+c$$$      if(a.eq.isec) then
+c$$$         rm_leg = a
+c$$$         if(b.eq.jsec) then
+c$$$            parent_leg = b
+c$$$            rec_leg = c
+c$$$         elseif(c.eq.jsec) then
+c$$$            parent_leg = c
+c$$$            rec_leg = b
+c$$$         endif
+c$$$      elseif(a.eq.jsec) then
+c$$$         rm_leg = a
+c$$$         if(b.eq.isec) then
+c$$$            parent_leg = b
+c$$$            rec_leg = c
+c$$$         elseif(c.eq.isec) then
+c$$$            parent_leg = c
+c$$$            rec_leg = b
+c$$$         endif
+c$$$      elseif((rm_leg.eq.0).or.(parent_leg.eq.0).or.(rm_leg.eq.parent_leg)) then
+c$$$         write(*,*) 'get_collinear_mapped_labels: '
+c$$$         write(*,*) 'Mapping tuple (abc) does not include'
+c$$$         write(*,*) 'particles defining the singular sector (ij) !'
+c$$$         write(*,*) 'a, b, c = ', a, b, c
+c$$$         write(*,*) 'i, j = ', isec, jsec
+c$$$         stop
+c$$$      endif
 c
 c     TODO: consistency check on (a,b,c) PDGs
 c
@@ -226,20 +267,20 @@ c        rescaling of mapped_labels
 
 c     Check that the recoiler mapped_flavour matches the recoiler particle
 c     chosen for the virtual process (reported in virtual_recoilers.inc)
-      do i=1,LEN_IREF
-         parent_from_v = IREF(1,i)
-         rec_from_v = IREF(2,i)
-         if((mapped_labels(parent_leg).eq.parent_from_v)
-     &        .and.(mapped_labels(rec_leg).eq.rec_from_v)) then
-            if(mapped_flavours(rec_leg).ne.UnderLying_leg_PDGs(rec_from_v))
-     &           then
-               write(*,*) 'get_collinear_mapped_labels: '
-               write(*,*) 'Recoiler flavour from mapped_flavours'
-               write(*,*) 'and recoiler flavour from virtual process does not match!'
-               write(*,*) mapped_flavours(rec_leg), UnderLying_leg_PDGs(rec_from_v)
-               stop
-            endif
-         endif
-      enddo
+c$$$      do i=1,LEN_IREF
+c$$$         parent_from_v = IREF(1,i)
+c$$$         rec_from_v = IREF(2,i)
+c$$$         if((mapped_labels(parent_leg).eq.parent_from_v)
+c$$$     &        .and.(mapped_labels(rec_leg).eq.rec_from_v)) then
+c$$$            if(mapped_flavours(rec_leg).ne.UnderLying_leg_PDGs(rec_from_v))
+c$$$     &           then
+c$$$               write(*,*) 'get_collinear_mapped_labels: '
+c$$$               write(*,*) 'Recoiler flavour from mapped_flavours'
+c$$$               write(*,*) 'and recoiler flavour from virtual process does not match!'
+c$$$               write(*,*) mapped_flavours(rec_leg), UnderLying_leg_PDGs(rec_from_v)
+c$$$               stop
+c$$$            endif
+c$$$         endif
+c$$$      enddo
       
       end

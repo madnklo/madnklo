8d156a4d70eabc74b96a10927afce8eeb22a3690
Giovanni Limatola <giovannilimatola@eduroam-1099.desy.de>
2024-12-12 17:15:36 +0100

    Draft version for integrated massive eikonal added to NLO_Vsub.f

--- Template/Fortran_tmp/src_to_common/NLO_V_sub.f
+++ Template/Fortran_tmp/src_to_common/NLO_V_sub.f
@@ -8,6 +8,7 @@
       double precision lam, eta, alpha, beta
       double precision Q, mk, ml
 
+      
       Q2=s+ml2+mk2
       lam=(s*v)**2d0
       Q = dsqrt(Q2)
@@ -23,20 +24,43 @@
          stop
       endif
 
-      INLO = - CCBLO * (
-     $ s/dsqrt(lam)*(
-     $ 1d0/2d0*dlog(eta)*(-dlog(s/mu**2)-dlog(lam**2/(mk2*ml2*Q2*s)))-
-     $ 1d0/4d0*dlog(eta)**2+dlog(eta)+2d0*dsqrt(lam)/s+
-     $ dlog(((Q+ml)**2-mk2)/(ml*(Q+ml)))*dlog((1d0-beta)/(1d0+beta))+
-     $ ddilog(-2d0*beta/(1d0-beta))-ddilog(2d0*beta/(1d0+beta))+
-     $ ddilog((1d0+beta)/2d0)-ddilog((1d0-beta)/2d0)-
-     $ ddilog((1d0-alpha)/(1d0-beta))+ddilog((1d0-alpha)/(1d0+beta))-
-     $ ddilog((1d0-beta)/(1d0+alpha))+ddilog((1d0+beta)/(1d0+alpha))+
-     $ ddilog(-2d0*beta/(alpha-beta))-ddilog(2d0*beta/(alpha+beta))-
-     $ ddilog(1d0-eta))+
-     $ 2d0-(2d0*ml2-Q2+s)*dlog((1d0-beta)/(1d0+beta))/dsqrt(lam)+
-     $ (2d0*mk2-2d0*Q2-s)*dlog(eta)/(2d0*dsqrt(lam))-
-     $ dlog(lam**2/(mk2*ml2*Q2*s)) - dlog(s/mu**2) )
-      
+c$$$      INLO = - CCBLO * (
+c$$$     $ s/dsqrt(lam)*(
+c$$$     $ 1d0/2d0*dlog(eta)*(-dlog(s/mu**2)-dlog(lam**2/(mk2*ml2*Q2*s)))-
+c$$$     $ 1d0/4d0*dlog(eta)**2+dlog(eta)+2d0*dsqrt(lam)/s+
+c$$$     $ dlog(((Q+ml)**2-mk2)/(ml*(Q+ml)))*dlog((1d0-beta)/(1d0+beta))+
+c$$$     $ ddilog(-2d0*beta/(1d0-beta))-ddilog(2d0*beta/(1d0+beta))+
+c$$$     $ ddilog((1d0+beta)/2d0)-ddilog((1d0-beta)/2d0)-
+c$$$     $ ddilog((1d0-alpha)/(1d0-beta))+ddilog((1d0-alpha)/(1d0+beta))-
+c$$$     $ ddilog((1d0-beta)/(1d0+alpha))+ddilog((1d0+beta)/(1d0+alpha))+
+c$$$     $ ddilog(-2d0*beta/(alpha-beta))-ddilog(2d0*beta/(alpha+beta))-
+c$$$     $ ddilog(1d0-eta))+
+c$$$     $ 2d0-(2d0*ml2-Q2+s)*dlog((1d0-beta)/(1d0+beta))/dsqrt(lam)+
+c$$$     $ (2d0*ml2-2d0*Q2-s)*dlog(eta)/(2d0*dsqrt(lam))-
+c$$$     $     dlog(lam**2/(mk2*ml2*Q2*s)) - dlog(s/mu**2) )
+
+
+
+      INLO = - 0.5d0*CCBLO*(8d0+(4d0*mk2*dlog((1d0-beta)/(1d0+beta)))/dsqrt(lam)
+     $     -(4d0*ml2*dlog((1d0-beta)/(1d0+beta)))/dsqrt(lam) - 
+     $     (2d0*s*dlog((1d0-beta)/(1d0+beta))*dlog((1d0-beta**2)/
+     $     (2d0*(1d0+alpha))))/dsqrt(lam)+(2d0*ml2*dlog(eta))/dsqrt(lam) - 
+     $     (2d0*Q2*dlog(eta))/dsqrt(lam)+(s*dlog(eta))/dsqrt(lam)-
+     $     (s*dlog(eta)**2)/(2d0*dsqrt(lam)) - 
+     $     2d0*dlog(lam**2/(mk2*ml2*Q2*s))-(s*dlog(eta)*
+     $     dlog(lam**2/(mk2*ml2*Q2*s)))/dsqrt(lam) - 
+     $     2d0*dlog(s/mu**2)-(s*dlog(eta)*dlog(s/mu**2))/dsqrt(lam)-
+     $     (2d0*s*ddilog((1d0-alpha)/(1d0-beta)))/dsqrt(lam) - 
+     $     (2d0*s*ddilog((1d0-beta)/2d0))/dsqrt(lam) -
+     $     (2d0*s*ddilog((1d0-beta)/(1d0+alpha)))/dsqrt(lam) + 
+     $     (2d0*s*ddilog((-2d0*beta)/(1d0-beta)))/dsqrt(lam) +
+     $     (2d0*s*ddilog((-2d0*beta)/(alpha-beta)))/dsqrt(lam) + 
+     $     (2d0*s*ddilog((1d0-alpha)/(1d0+beta)))/dsqrt(lam) -
+     $     (2d0*s*ddilog((2d0*beta)/(1d0+beta)))/dsqrt(lam) + 
+     $     (2d0*s*ddilog((1d0+beta)/2d0))/dsqrt(lam) +
+     $     (2d0*s*ddilog((1d0+beta)/(1d0+alpha)))/dsqrt(lam) - 
+     $     (2d0*s*ddilog((2d0*beta)/(alpha+beta)))/dsqrt(lam) -
+     $     (2d0*s*ddilog(1d0-eta))/dsqrt(lam))
+
       end
 

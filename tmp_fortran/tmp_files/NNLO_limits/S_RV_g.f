e13e6b5ad18d740776e2dbf3e7dc7d63bf641f00
Giovanni Limatola <giovannilimatola@MacBook-Pro-di-Giovanni-5.local>
2026-01-27 18:28:36 +0100

    First implementation of S_RV_g.f

--- tmp_fortran/tmp_files/NNLO_limits/S_RV_g.f
+++ tmp_fortran/tmp_files/NNLO_limits/S_RV_g.f
@@ -54,7 +54,12 @@ c     external
       common/c_mapped_quantities_s/mapped_labels,mapped_flavours,mapped_indices_shuff
       double precision xpb_to_ME(0:3,nexternal-1)
       DOUBLE PRECISION PMASS(NEXTERNAL)
+      double precision delta_s(-2:0)
+c     Label conventions according to Eq.(5.19) in 2212.11190
+c     with (c,d) ---> (l,m)
+      double precision ccBLO_ilm, ccBLO_iml, ccBLO_irl,ccBLO_ilr
       INCLUDE 'pmass.inc'
+      
 
 c
 c     initialise
@@ -196,6 +201,307 @@ c     sanity check
       endif
 c
       return
+ 999  ierr=1
+      return
+      end
+
+
+
+
+
+
+      function DELTA_S_RV_g(i,xs,xp,wgt,xj,xjB,nit,extra,wgt_chan,ierr)
+c     Delta single-soft limit S_(i) * Wsoft for RV
+c     it returns 0 if i is not a gluon
+      use sectors2_module
+      implicit none
+      include 'nexternal.inc'
+      INCLUDE 'coupl.inc'
+      include 'math.inc'
+      include 'damping_factors.inc'
+      include 'colored_partons.inc'
+      include 'leg_PDGs.inc'
+      include 'nsqso_born.inc'
+      INCLUDE 'input.inc'
+      INCLUDE 'run.inc'
+      double precision delta_s_rv_g(-2:0)
+      integer i,l,m,q,lb,mb,qb,ierr,nit,idum
+      double precision pref,M2tmp(-2:0),wgt,wgtpl,wgt_chan,xj,xjB,xjCS
+      double precision xs(nexternal,nexternal),xsb(nexternal-1,nexternal-1)
+      double precision BLO,ccBLO,triBLO,VLO(-2:0),ccVLO(-2:0),extra
+      double precision xp(0:3,nexternal),xpb(0:3,nexternal-1)
+      double precision sil,sim,slm,ml2,mm2,siq,smq,y,z,x,damp
+      double precision eik0,eik1(-2:0),eik2(-2:0)
+c     set logical doplot
+      logical doplot
+      common/cdoplot/doplot
+      double precision sCM
+      common/cscm/sCM
+      logical docut
+      integer %(proc_prefix_real)s_fl_factor
+      common/%(proc_prefix_real)s_flavour_factor/%(proc_prefix_real)s_fl_factor
+c     external
+      integer get_color_dipole_index
+      external get_color_dipole_index
+      double precision alphas,ans(0:NSQSO_BORN)
+      double precision alpha_qcd
+      double precision alphaZ
+      parameter(alphaZ=1d0)
+      integer, parameter :: HEL = - 1
+      double precision  %(proc_prefix_S_RV_g)s_GET_CCBLO
+      double precision  %(proc_prefix_S_RV_g)s_GET_CCVLO
+      double precision  %(proc_prefix_S_RV_g)s_GET_TRIBLO
+      integer %(proc_prefix_real)s_den
+      common/%(proc_prefix_real)s_iden/%(proc_prefix_real)s_den
+      integer %(proc_prefix_S_RV_g)s_den
+      common/%(proc_prefix_S_RV_g)s_iden/%(proc_prefix_S_RV_g)s_den
+      integer isec,jsec,ksec,lsec,iref
+      common/csecindices/isec,jsec,ksec,lsec,iref
+      integer underlying_leg_pdgs(nexternal-1)
+      common/c_U_PDGs/UNDERLYING_LEG_PDGS
+      integer mapped_labels(nexternal)
+      integer mapped_flavours(nexternal-1),mapped_indices_shuff(nexternal-1)
+      common/c_mapped_quantities_s/mapped_labels,mapped_flavours,mapped_indices_shuff
+      double precision xpb_to_ME(0:3,nexternal-1)
+      double precision xpb_to_ME_lm(0:3,nexternal-1),xpb_to_ME_lm(0:3,nexternal-1)
+      DOUBLE PRECISION PMASS(NEXTERNAL)
+      double precision delta_s(-2:0)
+c     Label conventions according to Eq.(5.19) in 2212.11190
+c     with (c,d) ---> (l,m)
+      double precision ccBLO_ilm, ccBLO_iml, ccBLO_irl,ccBLO_ilr
+      double precision xpb_lm(0:3,nexternal-1), xpb_ml(0:3,nexternal-1)
+      double precision xsb_lm(nexternal-1,nexternal-1), xsb_ml(nexternal-1,nexternal-1)
+      INCLUDE 'pmass.inc'
+      
+
+c
+c     initialise
+      DELTA_S_RV_g=0d0
+      M2tmp=0d0
+      ierr=0
+      damp=0d0
+      idum=0
+      xpb_to_ME=0d0
+      delta_s=0d0
+      ccBLO_ilm = 0d0
+      ccBLO_iml = 0d0
+      ccBLO_irl = 0d0
+      ccBLO_ilr = 0d
+      xpb_lm = 0d0
+      xpb_ml = 0d0
+c
+c     checks
+      if(leg_pdgs(i).ne.21)then
+         write(*,*)'Wrong pdgs in M2_S_RV_g',leg_pdgs(i)
+         stop
+      endif
+      if(.not.(i.eq.isec))then
+         write(*,*)'Wrong indices in M2_S_RV_g',i,isec
+         stop
+      endif
+c
+c     overall kernel prefix
+      ALPHAS=ALPHA_QCD(ASMZ,NLOOP,SCALE)
+      pref=-8d0*pi*alphas
+c
+c     eikonal double sum
+      do m=1,nexternal
+         if(.not.isNLOQCDparton(m))cycle
+         if(m.eq.i)cycle
+         do l=1,nexternal
+            if(.not.isNLOQCDparton(l))cycle
+            if(l.eq.i)cycle
+            if(l.eq.m)cycle
+c
+c     phase-space mapping according to l and m, at fixed radiation
+c     phase-space point: the singular kernel is in the same point
+c     as the single-real, ensuring numerical stability, while the
+c     underlying Born configuration is remapped
+C     Build B_lm^{(ilm)} and B_lm^{(iml)}
+c     The structure is such that we have
+c     Eik(xs)*(B_lm^{(ilm)}*theta(ilm)-B_lm^{(iml)}*theta(iml))
+c     Take care of this in applying kinematical cuts over Born kinematics
+c     Build  B_lm^{(ilm)}
+            call phase_space_CS_inv(i,l,m,xp,xpb_lm,nexternal,leg_PDGs,xjCS)
+            if(xjCS.eq.0d0)goto 999
+            call invariants_from_p(xpb_lm,nexternal-1,xsb_lm,ierr)
+            if(ierr.eq.1)goto 999
+c     Build  B_lm^{(iml)}            
+            call phase_space_CS_inv(i,m,l,xp,xpb_ml,nexternal,leg_PDGs,xjCS)
+            if(xjCS.eq.0d0)goto 999
+            call invariants_from_p(xpb_ml,nexternal-1,xsb_ml,ierr)
+            if(ierr.eq.1)goto 999
+c     possible cuts
+            IF((DOCUT(XPB_LM,NEXTERNAL-1,MAPPED_FLAVOURS,0)).and.(DOCUT(XPB_ML,NEXTERNAL-1,MAPPED_FLAVOURS,0))) CYCLE
+
+c     invariant quantities
+            sil=xs(i,l)
+            sim=xs(i,m)
+            slm=xs(l,m)
+            ml2=pmass(l)**2
+            mm2=pmass(m)**2
+c
+c     safety check
+            if(sil*sim.le.0d0)then
+               write(77,*)'Inaccuracy 1 in M2_S_RV_g',sil,sim
+               goto 999
+            endif
+            lb=mapped_indices_shuff(mapped_labels(l))
+            mb=mapped_indices_shuff(mapped_labels(m))
+c
+c     call colour-connected B^{(ilm)} and B^{(iml)}
+            ANS = 0d0
+            XPB_TO_ME_LM(0:3,MAPPED_INDICES_SHUFF(:))=XPB_LM(0:3,:)
+            call %(proc_prefix_S_RV_g)s_ME_ACCESSOR_HOOK(xpb_to_ME_lm,hel,alphas,ANS)
+            ccBLO_lm = %(proc_prefix_S_RV_g)s_GET_CCBLO(lb,mb)
+c
+            ANS = 0d0
+            XPB_TO_ME_ML(0:3,MAPPED_INDICES_SHUFF(:))=XPB_ML(0:3,:)
+            
+            call %(proc_prefix_S_RV_g)s_ME_ACCESSOR_HOOK(xpb_to_ME_ml,hel,alphas,ANS)
+            ccBLO_ml = %(proc_prefix_S_RV_g)s_GET_CCBLO(lb,mb)
+c
+c     eikonals
+            EIK0 =  SLM/(SIL*SIM) - ML2/SIL**2 - MM2/SIM**2
+            delta_s(-2) = delta_s(-2) + EIK0*2d0*CA*(ccBLO_ilm-ccBLO_iml)
+
+            if(abs(leg_pdgs(l)).le.6) then
+               gamma_l = gamma_q
+            elseif(leg_pdgs(l).eq.21) then
+               gamma_l = gamma_g
+            else
+               write(*,*) 'Error in evaluating gamma_c in delta_S_RV_g'
+               write(*,*) 'c, leg_pdgs(c) = ', l, leg_pdgs(l)
+               write(*,*) 'Exit...'
+               stop
+            endif
+            
+            delta_s(-1) = delta_s(-1) + EIK0*(ccBLO_ilm-ccBLO_iml)*(4d0*CA+gamma_l)
+            
+c     (c d e f) ---> (l m p q)
+            do p=1,nexternal
+               if(.not.isNLOQCDparton(p))cycle
+               if(p.eq.i.or.p.eq.l) cycle
+               do q=1,nexternal
+                  if(.not.isNLOQCDparton(q))cycle
+                  if(q.eq.i.or.q.eq.l.or.q.eq.p) cycle
+                  pb = mapped_indices_shuff(mapped_labels(p))
+                  qb = mapped_indices_shuff(mapped_labels(q))
+c     call invariants
+                  spq = xs(p,q)
+                  spm = xs(p,m)
+                  sbpm = xsb_lm(MAPPED_INDICES_SHUFF(l),MAPPED_INDICES_SHUFF(m)) 
+                  sbpq = xsb_lm(MAPPED_INDICES_SHUFF(p),MAPPED_INDICES_SHUFF(q))
+c     call quadruple-colour-connected Born
+                  call %(proc_prefix_S_RV_g)s_ME_ACCESSOR_HOOK(xpb_to_ME,hel,alphas,ANS)
+                  QUADBLO_pqlm = %(proc_prefix_S_RV_g)s_GET_QUADBLO(pb,qb,lb,mb)
+                  QUADBLO_pmlm = %(proc_prefix_S_RV_g)s_GET_QUADBLO(pb,mb,lb,mb)
+
+                  delta_s(-1) = delta_s(-1) + EIK0*(1d0/2d0*dlog(spq/sbpq)*QUADBLO_pqlm-dlog(spm/sbpm)*QUADBLO_pmlm)
+                  delta_s(0)  = delta_s(0) +  1d0/4d0*EIK0*(2d0*dlog(spm/sbpm)**2* QUADBLO_pmlm+dlog(spq/sbpq)**2*QUADBLO_pqlm)
+
+
+                  M2TMP(-2:0) = M2TMP(-2:0) + alphas/2d0/pi*delta_s(-2:0)
+
+
+c     Including correct multiplicity factor
+                  M2tmp(-2:0) = M2tmp(-2:0)*dble(%(proc_prefix_S_RV_g)s_den)/dble(%(proc_prefix_real)s_den)
+c
+c     damping factors
+c     TODO: adapt damping factors
+c            if(m.gt.2.and.l.gt.2)then
+c               y=sil/(sil+sim+slm)
+c               z=sim/(sim+slm)
+c               damp=((1d0-y)*(1d0-z))**alpha
+c            elseif(m.gt.2.and.l.le.2)then
+c               z=sim/(sim+slm)
+c               x=1d0 - sil/(sim+slm)
+c               damp=((1d0-z)*x)**alpha
+c            elseif(m.le.2.and.l.le.2)then
+c               x=1d0 - (sil+sim)/slm
+c               damp=x**alpha
+c            endif
+c            M2tmp(-2:0)=M2tmp(-2:0)*damp*xj
+            DELTA_S_RV_g(-2:0)=DELTA_S_RV_g(-2:0)+pref*M2tmp(-2:0)*WS_NLO*extra
+c
+c     plot
+c            wgtpl=-pref*M2tmp(0)*WS_NLO*extra*wgt/nit*wgt_chan
+c            wgtpl = wgtpl*%(proc_prefix_real)s_fl_factor
+c            if(doplot)call histo_fill(xpb,xsb,nexternal-1,mapped_flavours,wgtpl)
+c
+                  
+c     close q
+               enddo
+c     close p
+            enddo
+c     close m
+         enddo
+c     Sum over (k,c) ---> (l,k)
+         do k=1,nexternal
+            if(.not.isNLOQCDparton(k))cycle
+            if(k.eq.i) cycle
+            if(k.eq.l) cycle
+            if(k.eq.iref) cycle
+c     Build  B_lm^{(irk)}
+            call phase_space_CS_inv(i,iref,k,xp,xpb_rk,nexternal,leg_PDGs,xjCS)
+            if(xjCS.eq.0d0)goto 999
+            call invariants_from_p(xpb_rk,nexternal-1,xsb_rk,ierr)
+            if(ierr.eq.1)goto 999
+c     Build  B_lm^{(ikr)}            
+            call phase_space_CS_inv(i,k,iref,xp,xpb_kr,nexternal,leg_PDGs,xjCS)
+            if(xjCS.eq.0d0)goto 999
+            call invariants_from_p(xpb_kr,nexternal-1,xsb_kr,ierr)
+            if(ierr.eq.1)goto 999
+c     possible cuts
+            IF((DOCUT(XPB_kr,NEXTERNAL-1,MAPPED_FLAVOURS,0)).and.(DOCUT(XPB_rk,NEXTERNAL-1,MAPPED_FLAVOURS,0))) CYCLE
+c     invariant quantities
+            sik=xs(i,k)
+            sir=xs(i,iref)
+            skr=xs(k,iref)
+            mk2=pmass(k)**2
+            mr2=pmass(iref)**2
+
+c     safety check
+            if(sik*sir.le.0d0)then
+               write(77,*)'Inaccuracy 2 in M2_S_RV_g',sik,sir
+               goto 999
+            endif
+
+            kb=mapped_indices_shuff(mapped_labels(k))
+            rb=mapped_indices_shuff(mapped_labels(iref))
+c
+c     call colour-connected B^{(icr)} and B^{(irc)}
+            ANS = 0d0
+            XPB_TO_ME_kr(0:3,MAPPED_INDICES_SHUFF(:))=XPB_kr(0:3,:)
+            call %(proc_prefix_S_RV_g)s_ME_ACCESSOR_HOOK(xpb_to_ME_kr,hel,alphas,ANS)
+            ccBLO_kr = %(proc_prefix_S_RV_g)s_GET_CCBLO(kb,rb)
+c
+            ANS = 0d0
+            XPB_TO_ME_rk(0:3,MAPPED_INDICES_SHUFF(:))=XPB_rk(0:3,:)
+            call %(proc_prefix_S_RV_g)s_ME_ACCESSOR_HOOK(xpb_to_ME_rk,hel,alphas,ANS)
+            ccBLO_rk = %(proc_prefix_S_RV_g)s_GET_CCBLO(kb,rb)
+c     eikonals
+            EIK1 =  SKR/(SIK*SIR) - MK2/SIK**2 - MR2/SIR**2
+
+           M2TMP(-1) = delta_s(-1) + alphas/2d0/pi*pref*EIK1*gamma_l*(ccBLO_kr-ccBLO_rk)*WS_NLO*extra
+c     close k
+         enddo
+         
+c     close l
+      enddo
+
+c     apply flavour factor
+      DELTA_S_RV_g(-2:0) = DELTA_S_RV_g(-2:0) * %(proc_prefix_real)s_fl_factor
+c
+c     sanity check
+      if(abs(DELTA_S_RV_g(0)).ge.huge(1d0).or.isnan(DELTA_S_RV_g(0)))then
+         write(77,*)'Exception caught in finite part of DELTA_S_RV_g',DELTA_S_RV_g(0)
+         goto 999
+      endif
+c
+
+      
+            
  999  ierr=1
       return
       end

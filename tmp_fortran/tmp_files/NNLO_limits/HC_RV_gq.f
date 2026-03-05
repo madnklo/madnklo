7a2f80223df568e7ea3f3ecb2f018fa3c68b1d50
Giovanni Limatola <giovannilimatola@MacBook-Pro-di-Giovanni-5.local>
2026-03-05 16:31:39 +0100

    Commit for Delta_HC_RV_gq

--- tmp_fortran/tmp_files/NNLO_limits/HC_RV_gq.f
+++ tmp_fortran/tmp_files/NNLO_limits/HC_RV_gq.f
@@ -146,3 +146,331 @@ c
  999  ierr=1
       return
       end
+
+
+      SUBROUTINE DELTA_HC_RV_gq(ia,ib,ir,xs,xp,xsb,xpb,wgt,xj,nit,extra,wgt_chan,ierr,ret)
+c     collinear limit C_(ia,ib) * Wcollinear for RV
+c     this is meant to represent the full collinear
+c     for sector (ia,ib)
+      use sectors2_module
+      implicit none
+      include 'nexternal.inc'
+      INCLUDE 'coupl.inc'
+      include 'math.inc'
+      include 'damping_factors.inc'
+      include 'nsqso_born.inc'
+      include 'leg_PDGs.inc'
+      INCLUDE 'input.inc'
+      INCLUDE 'run.inc'
+      double precision ret(-2:0)
+      double precision DELTA_C_gq(-2:0),M2_tmp(-2:-0),DELTA_SC_gq(-2:0)
+      integer ia,ib,ir,ierr,nit,parent_leg
+      double precision pref,wgt,wgtpl,wgt_chan,xj,extra
+      double precision xs(nexternal,nexternal)
+      double precision BLO,KKBLO,VLO(-2:0),KKVLO(-2:0),EIK0,EIK1(-2:0)
+      double precision xp(0:3,nexternal),kt(0:3)
+      double precision xpb_abr(0:3,nexternal-1),xpb_bra(0:3,nexternal-1)
+      double precision xsb_abr(nexternal-1,nexternal-1),xsb_bra(nexternal-1,nexternal-1)
+      double precision sab,sar,sbr,x,y,xinit,logab,damp
+      double precision wa,wb,wr,mb2,mr2
+      double precision ANS(0:NSQSO_BORN)
+      integer, parameter :: hel = - 1
+      double precision alphas,alpha_qcd
+      double precision alphaz
+      parameter(alphaz=1d0)
+      double precision %(proc_prefix_HC_RV_gq)s_GET_KKBLO
+      double precision %(proc_prefix_HC_RV_gq)s_GET_KKVLO
+      double precision ddilog
+c     set logical doplot
+      logical doplot
+      common/cdoplot/doplot
+      double precision sCM
+      common/cscm/sCM
+      logical docut
+      integer %(proc_prefix_real)s_fl_factor
+      common/%(proc_prefix_real)s_flavour_factor/%(proc_prefix_real)s_fl_factor
+      integer %(proc_prefix_real)s_den
+     common/%(proc_prefix_real)s_iden/%(proc_prefix_real)s_den
+      integer %(proc_prefix_HC_RV_gq)s_den
+      common/%(proc_prefix_HC_RV_gq)s_iden/%(proc_prefix_HC_RV_gq)s_den
+      integer isec,jsec,ksec,lsec,iref
+      common/csecindices/isec,jsec,ksec,lsec,iref
+      integer underlying_leg_pdgs(nexternal-1)
+      common/c_U_PDGs/UNDERLYING_LEG_PDGS
+      integer mapped_labels(nexternal)
+      integer mapped_labels_abr(nexternal),mapped_labels_bra(nexternal)
+      common/c_mapped_labels/mapped_labels
+      integer l,m,lb,mb
+      double precision PtimesB
+c     initialise
+      ret=0d0
+      DELTA_C_gq_0=0d0
+      DELTA_SC_gq =0d0
+      M2_tmp=0d0
+      M2tmp_SC = 0d0
+      PtimesB = 0d0
+      ierr=0
+      damp=0d0
+c
+c     checks
+      if(.not.(abs(leg_pdgs(ib)).le.6.and.leg_pdgs(ia).eq.21))then
+         write(*,*)'Wrong pdgs in DELTA_HC_RV_gq',leg_pdgs(ia),leg_pdgs(ib)
+         stop
+      endif
+      if(.not.((ia.eq.isec.and.ib.eq.jsec).or.(ia.eq.jsec.and.ib.eq.isec)))then
+         write(*,*)'Wrong indices in DELTA_HC_RV_gq',ia,ib,isec,jsec
+         stop
+      endif
+c     overall kernel prefix
+      alphas=alpha_QCD(asmz,nloop,scale)
+      pref=8d0*pi*alphas
+c     invariant quantities
+      sab=xs(ia,ib)
+      sar=xs(ia,ir)
+      sbr=xs(ib,ir)
+      sabr = sar+sbr
+      mb2=pmass(ib)**2
+      mr2=pmass(ir)**2
+c     Energy fraction of the quark
+      x=sbr/(sar+sbr)
+      y=sab/(sab+sar+sbr)
+      xinit = 1d0 - sab/(sar+sbr)
+c     Evaluate momenta with different mappings
+c     at fixed real kinematic
+c     We need xbp^{(abr)}, xbp^{(bra)},xbp^{(abrprime)},
+      call phase_space_CS_inv(ib,ir,ia,xp,xpb_bra,nexternal,leg_PDGs,xjCS,mapped_labels_bra)
+      if(xjCS.eq.0d0)goto 999
+      call invariants_from_p(xpb_bra,nexternal-1,xsb_bra,ierr)
+      if(ierr.eq.1)goto 999
+      call phase_space_CS_inv(ia,ir,ib,xp,xpb_arb,nexternal,leg_PDGs,xjCS,mapped_labels_arb)
+      if(xjCS.eq.0d0)goto 999
+      call invariants_from_p(xpb_arb,nexternal-1,xsb_arb,ierr)
+      if(ierr.eq.1)goto 999
+c     double sum for Eq.(5.20) (c,d) ---> (l,m)
+c     Barred momenta with different mapping labels are needed
+c     No double-pole
+      M2tmp(-2) = 0d0
+c
+c     safety check
+      if(sab.le.0d0.or.sar+sbr.le.0d0.or.x.le.0d0.or.x.ge.1d0)then
+         write(77,*)'Inaccuracy 1 in DELTA_HC_RV_gq',sab,sar+sbr,x
+         goto 999
+      endif
+c     compute collinear limit of sector function
+      call get_wc_nlo(xs,isec,jsec,iref,alphaz,nexternal)
+c     (abr) mapped Born
+      ANS = 0d0
+      call %(proc_prefix_HC_RV_gq)s_ME_ACCESSOR_HOOK(xpb,hel,alphas,ANS)
+      BLO = ANS(0)
+c     Eikonal
+      eik0 = sbr/sab/sar
+
+c     P_{gq}^{\mu\nu}B_{\mu\nu}
+      PtimesB = CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*BLO)
+cccc  Last term in soft-collinear
+
+
+
+
+      
+      
+
+cccc
+c
+      do l=1,nexternal
+         if(.not.isNLOQCDparton(l))cycle
+         if(l.eq.ia) cycle
+         if(l.eq.ib) cycle
+         if((docut(xpb,nexternal-1,underlying_leg_pdgs,0)) goto 778
+
+         if(abs(leg_pdgs(l)).le.6) then
+            gamma_l = gamma_q
+            phi_l = phi_q
+         elseif(leg_pdgs(l).eq.21) then
+            gamma_l = gamma_g
+            phi_l = phi_g
+         else
+            write(*,*) 'Error in evaluating gamma_c and phi_c'
+            write(*,*)     'in delta_HC_RV_gq'
+            write(*,*) 'c, leg_pdgs(c) = ', l, leg_pdgs(l)
+            write(*,*) 'Exit...'
+            stop
+         endif
+c         
+         M2tmp(-1) = M2tmp(-1) + gamma_l*PtimesB*wc_NLO
+c
+         M2tmp(0) = M2tmp(0) + phi_l*PtimesB*wc_NLO
+
+         if(ia.eq.isec) then
+            M2tmp_SC(-1) = M2tmp_SC(-1) + gamma_l*BLO
+            M2tmp_SC(0) = M2tmp_SC(0) + phi_l*BLO
+         endif
+c     Term with rprime missing
+         ret(-2:0)=ret(-2:0)+alphas/2d0/pi/sab*M2tmp(-2:0)*wc_NLO-
+     $      alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+         
+c
+c     
+c     plot
+         do m=1,nexternal
+            if(.not.isNLOQCDparton(m))cycle
+            if(m.eq.ia) cycle
+            if(m.eq.ib) cycle
+            if(m.eq.l) cycle
+c     (ijr) ---> (ia ib ir)
+c     Barred invariants (abr)
+            lb = mapped_labels(l)
+            mb = mapped_labels(m)
+            sb_lm = xsb(lb,mb)
+            sb_bl = xsb(mapped_labels(ib),lb)
+c     Barred invariants (bra)
+            sb_bra_al = xsb_bra(mapped_labels_bra(ia),mapped_labels_bra(l))
+            sb_bra_ar = xsb_bra(mapped_labels_bra(ia),mapped_labels_bra(ir))
+c     Barred invariants (arb)
+            sb_arb_bl = xsb_arb(mapped_labels_arb(ib),mapped_labels_arb(l))
+            sb_arb_br = xsb_arb(mapped_labels_arb(ib),mapped_labels_arb(ir))
+c    Double real invariants
+            slm = xs(l,m)
+            sal = xs(ia,l)
+            sar = xs(ia,ir)
+            sbl = xs(ib,l)
+            sbr = xs(ib,ir)
+C     Define Born matrix elements for each pair (l,m)
+C     Born matrix elements mapped (abr)
+c     Spin-color correlated born-matrix element
+            CCBLO_lm=%(proc_prefix_HC_RV_gq)s_GET_CCBLO(lb,mb)
+c     Colour-correlated Born_{[ab]l}
+            CCBLO_parent_l=%(proc_prefix_HC_RV_gq)s_GET_CCBLO(mapped_labels(ib),lb)
+c
+            M2tmp(-1) = M2tmp(-1) - CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_lm*dlog(slm/sb_lm)
+
+            M2tmp(-1) = M2tmp(-1) + CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_parent_l*2d0*(dlog(sb_bl/sabr))
+
+            M2tmp(0) = M2tmp(0) + CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_lm*dlog(slm/sb_lm)**2
+
+            M2tmp(0) = M2tmp(0) - CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))CCBLO_parent_l*2d0*dlog(sb_bl/sabr)**2
+c            
+c           if(m.gt.2.and.l.gt.2)then
+c              y=sil/(sil+sim+slm)
+c              z=sim/(sim+slm)
+c              damp=((1d0-y)*(1d0-z))**alpha
+c           elseif(m.gt.2.and.l.le.2)then
+c              z=sim/(sim+slm)
+c              x=1d0 - sil/(sim+slm)
+c              damp=((1d0-z)*x)**alpha
+c           elseif(m.le.2.and.l.le.2)then
+c              x=1d0 - (sil+sim)/slm
+c              damp=x**alpha
+c     endif
+
+c     Soft-Collinear
+            if(ia.eq.isec) then
+               M2tmp_SC(-1) = M2tmp_SC(-1)-CCBLO_lm*dlog(slm/sb_lm)+
+     $              2d0*dlog(sb_bl/sbr)*CCBLO_parent_l
+               
+               M2tmp_SC(0) = M2tmp_SC(0)+CCBLO_lm*dlog(slm/sb_lm)**2+
+     $              2d0*(dlog(sbr/sabr)**2-dlog(sbl/sabr)**2)*CCBLO_parent_l
+            endif
+            
+            ret(-2:0)=ret(-2:0)+alphas/2d0/pi/sab*M2tmp(-2:0)*wc_NLO-
+     $      alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+
+            ret = ret *dble(%(proc_prefix_HC_RV_gq)s_den)/dble(%(proc_prefix_real)s_den)*%(proc_prefix_real)s_fl_factor*damp*pref*xj*extra
+c
+c     plot
+            wgtpl=-ret(0)*wgt/nit*wgt_chan
+            if(doplot)call histo_fill(xpb,xsb,nexternal-1,underlying_leg_pdgs,wgtpl)
+c
+ 778        if(docut(xpb_bra,nexternal-1,underlying_leg_pdgs,0))) goto 779
+c
+c     Barred invariants (bra)
+            sb_bra_al = xsb_bra(mapped_labels_bra(ia),mapped_labels_bra(l))
+            sb_bra_ar = xsb_bra(mapped_labels_bra(ia),mapped_labels_bra(ir))
+c     Mapped (bra) Born matrix element
+            ANS = 0d0
+            call %(proc_prefix_HC_RV_gq)s_ME_ACCESSOR_HOOK(xpb_bra,hel,alphas,ANS)
+c     The mother particle for the splitting [ab] ---> a b is ib            
+            CCBLO_parent_l_bra=%(proc_prefix_HC_RV_gq)s_GET_CCBLO(mapped_labels(ib),mapped_labels_bra(l)) 
+            M2tmp(-1) = M2tmp(-1) + CA/CF*CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_parent_l_bra*dlog(sar/sal)
+c
+            M2tmp(0) = M2tmp(0) + CA/CF*CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_parent_l_bra*(dlog(sb_bra_al/sb_bra_ar)**2-dlog(sar*sb_bra_al/sb_bra_ar/sal)**2)
+c
+            if(ia.eq.isec) then
+               M2tmp_SC(-1) = M2tmp_SC(-1) + CA/CF*CCBLO_parent_l_bra*dlog(sar/sal)
+               M2tmp_SC(0) = M2tmp_SC(0) + CA/CF*CCBLO_parent_l_bra*
+     $           (dlog(sb_bra_al/sb_bra_ar)**2-dlog(sar*sb_bra_al/sb_bra_ar/sal)**2)
+            endif
+            
+            ret(-2:0)=ret(-2:0)+alphas/2d0/pi/sab*M2tmp(-2:0)*wc_NLO-
+     $      alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+            ret = ret *dble(%(proc_prefix_HC_RV_gq)s_den)/dble(%(proc_prefix_real)s_den)*%(proc_prefix_real)s_fl_factor*damp*pref*xj*extra
+c     
+c     plot
+            wgtpl=-ret(0)*wgt/nit*wgt_chan
+            if(doplot)call histo_fill(xpb_bra,xsb_bra,nexternal-1,underlying_leg_pdgs,wgtpl)
+c     a <---> b
+ 779        if((docut(xpb_arb,nexternal-1,underlying_leg_pdgs,0)) cycle
+c     Barred invariants (arb)            
+            sb_arb_bl = xsb_arb(mapped_labels_arb(ib),mapped_labels_arb(l))
+            sb_arb_br = xsb_arb(mapped_labels_arb(ib),mapped_labels_arb(ir))
+c     Mapped (arb) Born matrix element
+            ANS = 0d0
+            call %(proc_prefix_HC_RV_gq)s_ME_ACCESSOR_HOOK(xpb_arb,hel,alphas,ANS)
+c     The mother particle for the splitting [ab] ---> a b is ib                        
+            CCBLO_parent_l_arb=%(proc_prefix_HC_RV_gq)s_GET_CCBLO(mapped_labels(ib),mapped_labels_arb(l)) 
+c
+            M2tmp(-1) = M2tmp(-1) + CA/CF*CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_parent_l_arb*dlog(sbr/sbl)
+c
+            M2tmp(0) = M2tmp(0) + CA/CF*CF*((1d0-x)+2d0*x/(1d0-x)*(1d0+1d0-x**alpha))*CCBLO_parent_l_arb*(dlog(sb_arb_bl/sb_arb_br)**2-dlog(sbr*sb_arb_bl/sb_arb_br/sbl)**2)
+c
+
+            if(ia.eq.isec) then      ! CHECK
+               M2tmp_SC(-1) = M2tmp_SC(-1)+(2d0*CF-CA)/CF*CCBLO_parent_l_arb*dlog(sbr/sbl)
+               M2tmp_SC(0) = M2tmp_SC(0)+(2d0*CF-CA)/CF*CCBLO_parent_l_arb*(dlog(sb_arb_bl/sb_arb_br)**2-dlog(sbr*sb_arb_bl/sb_arb_br/sal)**2)
+            endif
+            
+            ret(-2:0)=ret(-2:0)+alphas/2d0/pi/sab*M2tmp(-2:0)*wc_NLO-
+     $      alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+            ret = ret *dble(%(proc_prefix_HC_RV_gq)s_den)/dble(%(proc_prefix_real)s_den)*%(proc_prefix_real)s_fl_factor*damp*pref*xj*extra
+c     plot
+            wgtpl=-ret(0)*wgt/nit*wgt_chan
+            if(doplot)call histo_fill(xpb_arb,xsb_arb,nexternal-1,underlying_leg_pdgs,wgtpl)
+c
+         enddo
+      enddo
+
+
+c     Term with rprime missing      
+      if(ia.eq.isec) then
+         if((docut(xpb,nexternal-1,underlying_leg_pdgs,0)) goto 999
+         M2tmp_SC(-1) = M2tmp_SC(-1) + alphas/2d0/pi*gamma_q*BLO
+         M2tmp_SC(0) = M2tmp_SC(0) + alphas/2d0/pi*(phi_q-gamma_q*dlog(sCM/scale**2))*BLO
+         ret(-2:0)=ret(-2:0)-alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+         ret = ret *dble(%(proc_prefix_HC_RV_gq)s_den)/dble(%(proc_prefix_real)s_den)*%(proc_prefix_real)s_fl_factor*damp*pref*xj*extra
+c     plot
+         wgtpl=-ret(0)*wgt/nit*wgt_chan
+         if(doplot)call histo_fill(xpb,xsb,nexternal-1,underlying_leg_pdgs,wgtpl)
+
+         
+999     if((docut(xpb_arb,nexternal-1,underlying_leg_pdgs,0)) return
+        M2tmp_SC(-1) = M2tmp_SC(-1) - alphas/2d0/pi*gamma_q*BLO_arb
+        M2tmp_SC(0) = M2tmp_SC(0) - alphas/2d0/pi*(phi_q-gamma_q*dlog(sCM/scale**2))*BLO_arb
+        ret(-2:0)=ret(-2:0)-alphas/2d0/pi*2d0*CF*eik0*M2tmp_SC(-2:0)     
+         ret = ret *dble(%(proc_prefix_HC_RV_gq)s_den)/dble(%(proc_prefix_real)s_den)*%(proc_prefix_real)s_fl_factor*damp*pref*xj*extra
+c     plot
+         wgtpl=-ret(0)*wgt/nit*wgt_chan
+         if(doplot)call histo_fill(xpb_arb,xsb_arb,nexternal-1,underlying_leg_pdgs,wgtpl)
+      endif
+      endif
+c
+c
+c     sanity check      
+      if(abs(ret(0)).ge.huge(1d0).or.isnan(ret(0)))then
+         write(77,*)'Exception caught in DELTA_HC_RV_gq',ret(0)
+         goto 999
+      endif
+c
+      return
+ 999  ierr=1
+      return
+      end

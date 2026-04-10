37571b23a2714c565799ac75ad54cbd676a80695
yashasvee <yashime2000@gmail.com>
2026-04-10 16:34:58 +0200

    [RR] Cleaning up S_g - work in progress

--- Template/Fortran_tmp/src_to_common/sectors4_module.f90
+++ Template/Fortran_tmp/src_to_common/sectors4_module.f90
@@ -4,7 +4,7 @@ module sectors4_module
   double precision, public, parameter :: alpha_mod=2d0
   double precision, public, parameter :: alpha_mod_bar=1d0
   double precision, public :: W_NNLO, WSS_NNLO, WCC_NNLO, WSS_CC_NNLO
-  double precision, public :: Wbar_NLO, WS_NLO,  WSbar_NLO, WC_NLO, WCbar_NLO
+  double precision, public :: Wbar_NLO, WS_NLO, WSbar_NLO, WC_NLO, WCbar_NLO
   double precision, allocatable, dimension(:,:), public :: xs_mod
   double precision, allocatable, dimension(:,:), public :: sig2,hatsig2
   double precision, allocatable, dimension(:,:,:,:), public :: sigNNLO,hatsigNNLO
--- tmp_fortran/tmp_files/NNLO_limits/S_g-wip.f
+++ tmp_fortran/tmp_files/NNLO_limits/S_g-wip.f
@@ -1,26 +1,33 @@
-
-      
       double precision function M2_S_g(i,xs,xp,wgt,xj,xjB,nit,extra,wgt_chan,ierr)
-c     single-soft limit S_(i) * Zsoft
+c     single-soft limit S_(i) * Wsoft
 c     it returns 0 if i is not a gluon
+      use sectors4_module
       implicit none
       include 'nexternal.inc'
-      INCLUDE 'coupl.inc'
+      include 'coupl.inc'
       include 'math.inc'
       include 'damping_factors.inc'
       include 'colored_partons.inc'
       include 'leg_PDGs.inc'
       include 'nsqso_born.inc'
-      INCLUDE 'input.inc'
-      INCLUDE 'run.inc'      
-      integer i,l,m,lb,mb,ierr,nit,idum
-      double precision pref,M2tmp,wgt,wgtpl,wgt_chan,ZS_NLO,xj,xjB,xjCS
+      include 'input.inc'
+      include 'run.inc'
+      include 'pmass.inc'
+      integer i,j,l,m,ierr,nit
+      integer jb,lb,mb
+      integer jbb,lbb,mbb
+      logical isNLOmappedQCDparton(nexternal-1)
+      logical isLOmappedQCDparton(nexternal-2)
+      double precision pref,M2tmp,wgt,wgts(1),wgtpl,wgts,wgt_chan,xj,xjB,xjCS1,xjCS2
       double precision xs(nexternal,nexternal),xsb(nexternal-1,nexternal-1)
-      double precision BLO,ccBLO,extra
+      double precision xsbb(nexternal-2,nexternal-2)
+      double precision BLO,ccBLO,QUADBLO_mlml,extra
       double precision xp(0:3,nexternal),xpb(0:3,nexternal-1)
-      double precision sil,sim,slm,ml2,mm2,y,z,x,damp
-      integer mapped_labels(nexternal), mapped_flavours(NEXTERNAL)
-      logical ismappedQCDparton(nexternal-1)
+      double precision xpbb(0:3,nexternal-2)
+      double precision sil,sim,slm,sij,sjl,sjm,ml2,mm2,y,z,x,damp
+      double precision alphas,ans(0:NSQSO_BORN)
+      double precision alpha_qcd
+      double precision pmass(nexternal)
 c     set logical doplot
       logical doplot
       common/cdoplot/doplot
@@ -32,57 +39,48 @@ c     set logical doplot
 c     external
       integer get_color_dipole_index
       external get_color_dipole_index
-      double precision alphas,ans(0:NSQSO_BORN)
-      double precision alpha_qcd
       integer, parameter :: HEL = - 1
       double precision  %(proc_prefix_S_g)s_GET_CCBLO
       integer %(proc_prefix_rr)s_den
       common/%(proc_prefix_rr)s_iden/%(proc_prefix_rr)s_den
-      integer %(proc_prefix_S_g)s_den
-      common/%(proc_prefix_S_g)s_iden/%(proc_prefix_S_g)s_den
-      INTEGER ISEC,JSEC,KSEC,LSEC
-      COMMON/CSECINDICES/ISEC,JSEC,KSEC,LSEC
-      INTEGER UNDERLYING_LEG_PDGS(NEXTERNAL-1)
-      DOUBLE PRECISION PMASS(NEXTERNAL)
-      INCLUDE 'pmass.inc'
-      
+c      integer %(proc_prefix_S_g)s_den
+c      common/%(proc_prefix_S_g)s_iden/%(proc_prefix_S_g)s_den
+      integer isec,jsec,ksec,lsec,iref
+      common/cpartindices/isec,jsec,ksec,lsec,iref
+      integer asec,bsec,csec,dsec
+      common/csecindices/asec,bsec,csec,dsec
+      integer map1,map2
+      integer real_leg_pdgs(nexternal-1),Born_leg_pdgs(nexternal-2)
+      common/c_NNLO_U_PDGs/real_leg_pdgs,Born_leg_pdgs
+      integer real_mapped_labels(nexternal),Born_mapped_labels(nexternal-1)
+      common/c_NNLO_mapped_labels/real_mapped_labels,Born_mapped_labels
 c
 c     initialise
       M2_S_g=0d0
       M2tmp=0d0
       ierr=0
       damp=0d0
-      idum=0
 c
 c     return if not gluon
-      if(leg_pdgs(I).ne.21)return
+      if(leg_pdgs(i).ne.21)return
 c
 c     safety check on PDGs
-      IF(SIZE(LEG_PDGS).NE.NEXTERNAL)THEN
-        WRITE(*,*) 'M2_S_g:'
-        WRITE(*,*) 'Wrong dimension for leg_PDGs',SIZE(LEG_PDGS),NEXTERNAL
-        STOP
-      ENDIF
-c
-c     get PDGs
-c      CALL GET_BORN_PDGS(ISEC,JSEC,NEXTERNAL-1,BORN_LEG_PDGS)
-      call GET_UNDERLYING_PDGS(ISEC,JSEC,KSEC,LSEC,NEXTERNAL-1,UNDERLYING_LEG_PDGS)
-
-      CALL GET_SOFT_MAPPED_LABELS(I,NEXTERNAL,LEG_PDGS,MAPPED_LABELS,MAPPED_FLAVOURS,ISMAPPEDQCDPARTON)
-c
-c     call Z soft
-      if(i.eq.isec) then
-         CALL GET_ZS_NLO(XS,SCM,ISEC,JSEC,ZS_NLO,IERR)
-      elseif(i.eq.jsec) then
-         CALL GET_ZS_NLO(XS,SCM,JSEC,ISEC,ZS_NLO,IERR)
-      else
-         write(*,*)'In M2_S_g i should be = isec or = jsec',i,isec,jsec
-         stop
+      if(size(leg_pdgs).ne.nexternal)then
+        write(*,*) 'M2_s_g:'
+        write(*,*) 'Wrong dimension for leg_pdgs',size(leg_pdgs),nexternal
+        stop
       endif
-      if(ierr.eq.1)goto 999
+c
+c     call soft limit of sector function according to eq. (C.51)
+      call get_sig2(xs,alpha_mod,nexternal)
+      call get_WS_NLO(asec,bsec)
+      call get_sig2(xsb,alpha_mod_bar,nexternal-1)
+      map1=real_mapped_labels(csec)
+      map2=real_mapped_labels(dsec)
+      call get_wbar_nlo(map1,map2)
 c
 c     overall kernel prefix
-      ALPHAS=ALPHA_QCD(ASMZ,NLOOP,SCALE)
+      alphas=alpha_qcd(asmz,nloop,scale)
       pref=-8d0*pi*alphas
 c
 c     eikonal double sum
@@ -98,26 +96,27 @@ c
             mb=mapped_labels(m)
 c
 c         check labels and pdgs
-          IF(.NOT.(ISMAPPEDQCDPARTON(LB).AND.ISMAPPEDQCDPARTON(MB)))THEN
-            WRITE(*,*)'Wrong indices 1 in M2_S_g',LB,MB
-            STOP
-          ENDIF
-          IF(leg_pdgs(l).ne.UnderLying_leg_pdgs(lb).or.leg_pdgs(m).ne.UnderLying_leg_pdgs(mb))THEN
-            WRITE(*,*)'Wrong indices 2 in M2_S_g',L,M,LB,MB
-            STOP
-          ENDIF
+          if(.not.(ismappedqcdparton(lb).and.ismappedqcdparton(mb)))then
+            write(*,*)'wrong indices 1 in M2_S_g',lb,mb
+            stop
+          endif
+          if(leg_pdgs(l).ne.underlying_leg_pdgs(lb).or.leg_pdgs(m).ne.underlying_leg_pdgs(mb))then
+            write(*,*)'wrong indices 2 in M2_S_g',l,m,lb,mb
+            stop
+          endif
 c
 c     phase-space mapping according to l and m, at fixed radiation
 c     phase-space point: the singular kernel is in the same point
-c     as the single-real, ensuring numerical stability, while the
+c     as the double-real, ensuring numerical stability, while the
 c     underlying Born configuration is remapped
-            call phase_space_CS_inv(i,l,m,xp,xpb,nexternal,leg_PDGs,xjCS)
-            if(xjCS.eq.0d0)goto 999
-            call invariants_from_p(xpb,nexternal-1,xsb,ierr)
+            call phase_space_CS_inv(i,l,m,xp,xpb,nexternal,leg_PDGs,xjCS1,real_mapped_labels)
+            call phase_space_CS_inv(jb,lb,mb,xpb,xpbb,nexternal-1,real_leg_PDGs,xjCS2,Born_mapped_labels)
+            if(xjCS1.eq.0d0.or.xjCS2.eq.0d0)goto 999
+            call invariants_from_p(xpbb,nexternal-2,xsbb,ierr)
             if(ierr.eq.1)goto 999
 c
 c     possible cuts
-            IF(DOCUT(XPB,NEXTERNAL-1,UNDERLYING_LEG_PDGS,0))CYCLE
+            if(docut(xpb,nexternal-1,underlying_leg_pdgs,0))cycle
 c
 c     invariant quantities
             sil=xs(i,l)
@@ -137,8 +136,8 @@ c     call colour-connected Born
             ccBLO = %(proc_prefix_S_g)s_GET_CCBLO(lb,mb)
 c
 c     eikonal
-            M2TMP=SLM/(SIL*SIM) - ML2/SIL**2 - MM2/SIM**2
-            M2TMP = CCBLO*M2TMP
+            M2tmp = slm/(sil*sim) - ml2/sil**2 - mm2/sim**2
+            M2tmp = ccBLO*M2tmp
 c     Including correct multiplicity factor
             M2tmp = M2tmp*dble(%(proc_prefix_S_g)s_den)/dble(%(proc_prefix_rr)s_den)
 c
@@ -149,21 +148,22 @@ c     damping factors
                damp=((1d0-y)*(1d0-z))**alpha
             elseif(m.gt.2.and.l.le.2)then
                z=sim/(sim+slm)
-               x=1d0 - sil/(sim+slm)
+               x=1d0-sil/(sim+slm)
                damp=((1d0-z)*x)**alpha
             elseif(m.le.2.and.l.le.2)then
-               x=1d0 - (sil+sim)/slm
+               x=1d0-(sil+sim)/slm
                damp=x**alpha
             endif
             M2tmp=M2tmp*damp*xj
-            M2_S_g=M2_S_g+pref*M2tmp*ZS_NLO*extra
+            M2_S_g=M2_S_g+pref*M2tmp*WS_NLO*Wbar_NLO*extra
 c
 c     plot
-            wgtpl=-pref*M2tmp*ZS_NLO*extra*wgt/nit*wgt_chan
-            wgtpl = wgtpl*%(proc_prefix_rr)s_fl_factor
-            if(doplot)call histo_fill(xpb,xsb,nexternal-1,wgtpl)
+            wgtpl=-pref*M2tmp*extra*wgt/nit*wgt_chan
+            wgts = wgtpl
+c            if(doplot)call histo_fill(xpb,xsb,nexternal-1,wgtpl)
+            if(doplot)call analysis_fill(xpb,xsb,nexternal-1,real_leg_pdgs,wgts)
 c
-         enddo 
+         enddo
       enddo
 c
 c     apply flavour factor

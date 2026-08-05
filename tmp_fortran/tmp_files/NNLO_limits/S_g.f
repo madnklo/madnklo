ddea77eef63fbea10cbdd94d33cd8fc0225a334b
yashasvee <yashime2000@gmail.com>
2026-08-05 16:06:29 +0200

    [RR] Fixes in S_g according to dropbox

--- tmp_fortran/tmp_files/NNLO_limits/S_g.f
+++ tmp_fortran/tmp_files/NNLO_limits/S_g.f
@@ -13,18 +13,17 @@ c     it returns 0 if i is not a gluon
       include 'nsqso_born.inc'
       include 'input.inc'
       include 'run.inc'
-      integer i,j,l,m,ierr,nit
-      integer jb,lb,mb
-      integer jbb,lbb,mbb
+      integer i,j,l,m,ierr,nit,a,b,c
+      integer jb,lb,mb,ab,cb,bb
       logical isNLOmappedQCDparton(nexternal-1)
       logical isLOmappedQCDparton(nexternal-2)
       double precision pref,M2tmp,wgt,wgts(1),wgtpl,wgt_chan,xj,xjB,xjCS1,xjCS2
       double precision xs(nexternal,nexternal),xsb(nexternal-1,nexternal-1)
       double precision xsbb(nexternal-2,nexternal-2)
-      double precision BLO,ccBLO,extra
+      double precision BLO,ccBLO,extra,ccRNLO
       double precision xp(0:3,nexternal),xpb(0:3,nexternal-1)
       double precision xpbb(0:3,nexternal-2)
-      double precision sil,sim,slm,sij,sjl,sjm,ml2,mm2,y,z,x,damp
+      double precision sic,sac,sia,sbc,sib,sab,ml2,mm2,y,z,x,damp
       double precision alphas,ans(0:NSQSO_BORN)
       double precision alpha_qcd
       double precision pmass(nexternal)
@@ -62,7 +61,7 @@ c     initialise
       M2_S_g=0d0
       M2tmp=0d0
       ierr=0
-      damp=0d0
+      damp=1d0
 c
 c     return if not gluon
       if(leg_pdgs(i).ne.21)return
@@ -81,30 +80,27 @@ c
 c     overall kernel prefix
       alphas=alpha_qcd(asmz,nloop,scale)
       pref=-8d0*pi*alphas
+
+      a = csec
+      b = dsec
+      ab=real_mapped_labels(a)
+      bb=real_mapped_labels(b)
 c
-c     eikonal double sum
-      do m=1,nexternal
-         if(.not.isnnloQCDparton(m))cycle
-         if(m.eq.i)cycle
-         do l=1,nexternal
-            if(.not.isnnloQCDparton(l))cycle
-            if(l.eq.i)cycle
-            if(l.eq.m)cycle
+c     eikonal sum
+      do c=1,nexternal
+         if(.not.isnnloQCDparton(c))cycle
+         if(c.eq.i.or.c.eq.a.or.c.eq.b)cycle
 c
-            lb=real_mapped_labels(l)
-            mb=real_mapped_labels(m)
+         cb=real_mapped_labels(c)
 c
-c     phase-space mapping according to l and m, at fixed radiation
-c     phase-space point: the singular kernel is in the same point
-c     as the double-real, ensuring numerical stability, while the
-c     underlying Born configuration is remapped
-            call phase_space_CS_inv(i,l,m,xp,xpb,nexternal,leg_PDGs,xjCS1,real_mapped_labels)
-            if(xjCS1.eq.0d0)goto 999
-            call invariants_from_p(xpb,nexternal-1,xsb,ierr)
-            if(ierr.eq.1)goto 999
+c     mapping 1: [ica]
+         call phase_space_CS_inv(i,c,a,xp,xpb,nexternal,leg_PDGs,xjCS1,real_mapped_labels)
+         if(xjCS1.eq.0d0)goto 999
+         call invariants_from_p(xpb,nexternal-1,xsb,ierr)
+         if(ierr.eq.1)goto 999
 c
 c     possible cuts
-            if(docut(xpb,nexternal-1,real_leg_pdgs,0))cycle
+            if(docut(xpb,nexternal-1,real_leg_pdgs,1))cycle
 c
 c     call Wbar
             call get_sig2(xsb,alpha_mod_bar,nexternal-1)
@@ -113,53 +109,124 @@ c     call Wbar
             call get_wbar_nlo(map1,map2)
 c
 c     invariant quantities
-            sil=xs(i,l)
-            sim=xs(i,m)
-            slm=xs(l,m)
-            ml2=pmass(l)**2
-            mm2=pmass(m)**2
+            sac=xs(a,c)
+            sic=xs(i,c)
+            sia=xs(i,a)
+            ! ml2=pmass(l)**2
+            ! mm2=pmass(m)**2
 c
 c     safety check
-            if(sil*sim.le.0d0)then
-               write(77,*)'Inaccuracy 1 in M2_S_g',sil,sim
+            if(sic*sia.le.0d0)then
+               write(77,*)'Inaccuracy 1 in M2_S_g',sic,sia
                goto 999
             endif
 c
 c     call colour-connected Born
             call %(proc_prefix_S_g)s_ME_ACCESSOR_HOOK(xpb,hel,alphas,ANS)
-            ccBLO = %(proc_prefix_S_g)s_GET_CCBLO(lb,mb)
+            ccRNLO = %(proc_prefix_S_g)s_GET_CCBLO(cb,ab)
 c
 c     eikonal
-            M2tmp = slm/(sil*sim) - ml2/sil**2 - mm2/sim**2
-            M2tmp = ccBLO*M2tmp
-c     Including correct multiplicity factor
-            M2tmp = M2tmp*dble(%(proc_prefix_S_g)s_den)/dble(%(proc_prefix_rr)s_den)
+            M2tmp = M2tmp + 2d0*sac/sic/sia*ccRNLO*wbar_nlo
+
+      enddo
+c
+      do c=1,nexternal
+         if(.not.isnnloQCDparton(c))cycle
+         if(c.eq.i.or.c.eq.a.or.c.eq.b)cycle
+c
+         cb=real_mapped_labels(c)
+c
+c     mapping 2: [icb]
+         call phase_space_CS_inv(i,c,b,xp,xpb,nexternal,leg_PDGs,xjCS1,real_mapped_labels)
+         if(xjCS1.eq.0d0)goto 999
+         call invariants_from_p(xpb,nexternal-1,xsb,ierr)
+         if(ierr.eq.1)goto 999
+c
+c     possible cuts
+            if(docut(xpb,nexternal-1,real_leg_pdgs,1))cycle
+c
+c     call Wbar
+            call get_sig2(xsb,alpha_mod_bar,nexternal-1)
+            map1=real_mapped_labels(csec)
+            map2=real_mapped_labels(dsec)
+            call get_wbar_nlo(map1,map2)
+c
+c     invariant quantities
+            sbc=xs(b,c)
+            sic=xs(i,c)
+            sib=xs(i,b)
+c
+c     safety check
+            if(sic*sia.le.0d0)then
+               write(77,*)'Inaccuracy 1 in M2_S_g',sic,sia
+               goto 999
+            endif
+c
+c     call colour-connected Born
+            call %(proc_prefix_S_g)s_ME_ACCESSOR_HOOK(xpb,hel,alphas,ANS)
+            ccRNLO = %(proc_prefix_S_g)s_GET_CCBLO(cb,bb)
+c
+c     eikonal
+            M2tmp = M2tmp + 2d0*sbc/sic/sib*ccRNLO*wbar_nlo
+c
 c
 c     damping factors
-            if(m.gt.2.and.l.gt.2)then
-               y=sil/(sil+sim+slm)
-               z=sim/(sim+slm)
-               damp=((1d0-y)*(1d0-z))**alpha
-            elseif(m.gt.2.and.l.le.2)then
-               z=sim/(sim+slm)
-               x=1d0-sil/(sim+slm)
-               damp=((1d0-z)*x)**alpha
-            elseif(m.le.2.and.l.le.2)then
-               x=1d0-(sil+sim)/slm
-               damp=x**alpha
-            endif
-            M2tmp=M2tmp*damp*xj
-            M2_S_g=M2_S_g+pref*M2tmp*ws_nlo*wbar_nlo*extra
+            ! if(m.gt.2.and.l.gt.2)then
+            !    y=sil/(sil+sim+slm)
+            !    z=sim/(sim+slm)
+            !    damp=((1d0-y)*(1d0-z))**alpha
+            ! elseif(m.gt.2.and.l.le.2)then
+            !    z=sim/(sim+slm)
+            !    x=1d0-sil/(sim+slm)
+            !    damp=((1d0-z)*x)**alpha
+            ! elseif(m.le.2.and.l.le.2)then
+            !    x=1d0-(sil+sim)/slm
+            !    damp=x**alpha
+            ! endif
+
 c
 c     plot
-            wgtpl=-pref*M2tmp*extra*wgt/nit*wgt_chan
+            wgtpl=+pref*M2tmp*extra*wgt/nit*wgt_chan
             wgts = wgtpl
 c            if(doplot)call histo_fill(xpb,xsb,nexternal-1,wgtpl)
             if(doplot)call analysis_fill(xpb,xsb,nexternal-1,real_leg_pdgs,wgts)
 c
-         enddo
       enddo
 c
+c     mapping 3: [iba]
+      call phase_space_CS_inv(i,b,a,xp,xpb,nexternal,leg_PDGs,xjCS1,real_mapped_labels)
+      if(xjCS1.eq.0d0)goto 999
+      call invariants_from_p(xpb,nexternal-1,xsb,ierr)
+      if(ierr.eq.1)goto 999
+c
+c     call Wbar
+      call get_sig2(xsb,alpha_mod_bar,nexternal-1)
+      map1=real_mapped_labels(csec)
+      map2=real_mapped_labels(dsec)
+      call get_wbar_nlo(map1,map2)
+c
+c     invariant quantities
+      sab=xs(a,b)
+      sia=xs(i,a)
+      sib=xs(i,b)
+c
+c     safety check
+      if(sib*sia.le.0d0)then
+            write(77,*)'Inaccuracy 1 in M2_S_g',sib,sia
+            goto 999
+      endif
+c
+c     call colour-connected Born
+      call %(proc_prefix_S_g)s_ME_ACCESSOR_HOOK(xpb,hel,alphas,ANS)
+      ccRNLO = %(proc_prefix_S_g)s_GET_CCBLO(ab,bb)
+c
+c     eikonal
+      M2tmp = M2tmp + 2d0*sab/sia/sib*ccRNLO*wbar_nlo
+c
+c     Including correct multiplicity factor
+      M2tmp = M2tmp*dble(%(proc_prefix_S_g)s_den)/dble(%(proc_prefix_rr)s_den)
+      M2tmp=M2tmp*damp*xj
+      M2_S_g=M2_S_g+pref*M2tmp*ws_nlo*extra
 c     apply flavour factor
       M2_S_g = M2_S_g * %(proc_prefix_rr)s_fl_factor
 c
